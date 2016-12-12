0b4e1860652de978b01ffee3d0f4c64d04a7f8e2
akb46mayu <bofansunrise@gmail.com>
2016-12-12 12:48:18 -0500

    Create le131_palindrome_partitioning.py

---
+++ String/le131_palindrome_partitioning.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+    
+        result =[]
+        self.partitionRecur(result, [], s, 0)
+        return result
+    def partitionRecur(self, result, cur, s, i):
+        
+        if i == len(s):
+            result.append(list(cur))
+        else:
+            
+            for j in xrange(i,len(s)):
+                if self.isPalindrome(s[i:j+1]):
+                    cur.append(s[i:j+1])
+                    self.partitionRecur(result, cur, s, j+1)
+                    cur.pop()
+    
+    
+    
+    def isPalindrome(self, s):
+        for i in xrange(len(s)/2):
+            if s[i]!=s[-i-1]:
+                return False
+                
+        return True
