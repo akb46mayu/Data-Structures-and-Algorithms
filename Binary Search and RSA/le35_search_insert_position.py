e2a10ad4d3602978737381f0d9d0a8dab961176e
akb46mayu <bofansunrise@gmail.com>
2016-12-13 16:21:41 -0500

    Create le35_search_insert_position.py

---
+++ Binary Search and RSA/le35_search_insert_position.py
@@ -0,0 +1,38 @@
+class Solution(object): # method 1
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        start, end = 0, len(nums)-1
+        while start + 1< end:
+            mid = start + (end - start)/2
+            if nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if nums[start] >= target:
+            return start
+        if nums[end] >= target:
+            return end
+        return len(nums)
+        
+class Solution2(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end)/2
+            if nums[mid] >= target:
+                end = mid - 1
+                
+            else:
+                start = mid + 1
+        return start
