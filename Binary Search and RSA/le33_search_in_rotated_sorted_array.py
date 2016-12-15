2c7658ac4f2e5c3a1dab19212c99c18abb508dec
akb46mayu <bofansunrise@gmail.com>
2016-12-15 18:20:29 -0500

    Create le33_search_in_rotated_sorted_array.py

---
+++ Binary Search and RSA/le33_search_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end)/2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > nums[start]:
+                if nums[mid] >= target and target >= nums[start]:
+                    end = mid
+                else:
+                    start = mid
+            else:
+                if nums[mid] <= target and target <= nums[end]:
+                    start = mid
+                else:
+                    end = mid
+        if nums[start] == target:
+            return start
+        elif nums[end] == target:
+            return end
+        else:
+            return -1
