5af437185457147d5bb20006443b31a36fd148c6
akb46mayu <bofansunrise@gmail.com>
2016-12-15 17:08:35 -0500

    Update le162_find_peak_element.py

--- Binary Search and RSA/le162_find_peak_element.py
+++ Binary Search and RSA/le162_find_peak_element.py
@@ -1,4 +1,25 @@
-class Solution(object):
+class Solution(object): # I prefer to this solution
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        n = len(nums) - 1
+        start, end = 0, n
+        
+        while start <= end:
+            mid = (start + end)/2
+            if (mid==0 or nums[mid] > nums[mid-1]) and (mid == n or nums[mid] > nums[mid+1]):
+                return mid
+            elif (mid!=0 and nums[mid] < nums[mid-1]):
+                end = mid - 1
+            else:
+                start = mid + 1
+        return start
+    
+    
+class Solution2(object):
     def findPeakElement(self, nums):
         """
         :type nums: List[int]
