1e254eec8cfbcbd07b9f6c64177ba39a33627ae7
akb46mayu <bofansunrise@gmail.com>
2016-12-18 16:50:02 -0500

    Create le144_binarytree_preorder_traversal.py

---
+++ BinaryTree and Divide and Conquer/le144_binarytree_preorder_traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """ 
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        results = []
+        self.subPreorderTraversal(root, results)
+        return results
+    
+    def subPreorderTraversal(self, root, results):
+        
+        if root is None:
+            return 
+        results.append(root.val)
+        self.subPreorderTraversal(root.left, results)
+        self.subPreorderTraversal(root.right, results)
