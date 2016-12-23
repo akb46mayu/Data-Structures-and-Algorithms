df673a1a6ffc4ad672d744117a0f0d79c668ec90
akb46mayu <bofansunrise@gmail.com>
2016-12-23 16:41:24 -0500

    Create le83_remove_duplicates_from_linkedlist.py

---
+++ BinaryTree and Divide and Conquer/le83_remove_duplicates_from_linkedlist.py
@@ -0,0 +1,30 @@
+"""
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return None
+        p = head
+        while p.next != None:
+            if p.val == p.next.val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return head
