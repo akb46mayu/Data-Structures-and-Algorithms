e573f524680d57a62e5d379bc257508308993bdf
akb46mayu <bofansunrise@gmail.com>
2016-12-25 18:17:19 -0500

    Create le206_reverse_linkedlist.py

---
+++ LinkedList/le206_reverse_linkedlist.py
@@ -0,0 +1,22 @@
+"""
+Reverse a singly linked list.
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        while head:
+            cur = head
+            head = head.next
+            cur.next = prev
+            prev = cur
+        return prev
