c6928908230fd2e79dc0c63171723817a350575f
akb46mayu <bofansunrise@gmail.com>
2016-12-25 22:34:02 -0500

    Create le86_partition_list.py

---
+++ LinkedList/le86_partition_list.py
@@ -0,0 +1,40 @@
+"""
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        leftDummy = ListNode(-1)
+        rightDummy = ListNode(-1)
+        left, right = leftDummy, rightDummy
+        while head:
+            if head.val < x:
+                left.next = head
+                left = head
+            else:
+                right.next = head
+                right = head
+            head = head.next
+        left.next = rightDummy.next
+        right.next = None
+        
+        return leftDummy.next
