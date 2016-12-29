7c0604a045afbe3948b0865a1300491bcee9dd01
akb46mayu <bofansunrise@gmail.com>
2016-12-29 17:17:09 -0500

    Update le128_longest_consecutive_sequence.py

--- Other Data Structures_qstackheap/le128_longest_consecutive_sequence.py
+++ Other Data Structures_qstackheap/le128_longest_consecutive_sequence.py
@@ -22,7 +22,6 @@ class Solution(object):
         low = nums[0]
         templen = 1
         maxlen = 1
-        
         for val in nums:
             if val == low:
                 continue
@@ -33,4 +32,28 @@ class Solution(object):
             else:
                 templen = 1
             low = val
+        return maxlen
+
+    class Solution2(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums is None or len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return 1
+        nums.sort()
+        n = len(nums)
+        i = 0
+        maxlen = 1
+        while i < n:
+            tmplen = 1
+            while i<=n-2 and nums[i+1] - nums[i] <=1:
+                if nums[i+1] - nums[i] == 1:
+                    tmplen+=1
+                i = i + 1
+            maxlen = max(maxlen, tmplen)
+            i = i + 1
         return maxlen
