d353395f8e8839b9e0515268d84c44966ede4eff
akb46mayu <bofansunrise@gmail.com>
2016-12-29 22:51:42 -0500

    Create le62_unique_paths.py

---
+++ Dynamic Programming/le62_unique_paths.py
@@ -0,0 +1,30 @@
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+"""
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        f = [[0]*n]*m
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j==0:
+                    f[i][j] = 1
+                else:
+                    f[i][j] = f[i-1][j] + f[i][j-1]
+        return f[m-1][n-1]
