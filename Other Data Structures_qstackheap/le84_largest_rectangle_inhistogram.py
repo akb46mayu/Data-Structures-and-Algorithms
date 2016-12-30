1d79b97635542e6fa33c7cda8ac37bb2fb9b0a36
akb46mayu <bofansunrise@gmail.com>
2016-12-29 21:45:35 -0500

    Create le84_largest_rectangle_inhistogram.py

---
+++ Other Data Structures_qstackheap/le84_largest_rectangle_inhistogram.py
@@ -0,0 +1,37 @@
+"""
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+For example,
+Given heights = [2,1,5,6,2,3],
+return 10.
+"""
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        if heights is None or len(heights) == 0:
+            return 0
+        
+        area = 0
+        n= len(heights)
+        stack = []
+        for i in range(n+1):#  for idx i, compute i-1's height according to its leftmose(stack[-1]) and rightmost(i) min pillar
+            if i == n:
+                curHeight = -1
+            else:
+                curHeight = heights[i]
+            while stack and curHeight <= heights[stack[-1]]:
+                h = heights[stack.pop()]
+                if stack:
+                    w = i-1-stack[-1]
+                else:
+                    w = i
+                area = max(area, w*h)
+            stack.append(i)
+        return area
