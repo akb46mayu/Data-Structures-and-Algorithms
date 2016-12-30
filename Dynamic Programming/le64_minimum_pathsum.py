5f94ddfc7e662a5203f34ee9c11c2cf36cea2487
akb46mayu <bofansunrise@gmail.com>
2016-12-29 22:37:48 -0500

    Update le64_minimum_pathsum.py

--- Dynamic Programming/le64_minimum_pathsum.py
+++ Dynamic Programming/le64_minimum_pathsum.py
@@ -1,20 +1,45 @@
+"""
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+"""
+
 class Solution(object):
     def minPathSum(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        numrows = len(grid)
-        numcols = len(grid[0])
-        
-        for i in xrange(numrows):
-            for j in xrange(numcols):
-                if i==0 and j>0:
-                    grid[i][j]+= grid[i][j-1] 
-                elif j==0 and i>0:
-                    grid[i][j]+= grid[i-1][j]
-                elif j>0 and i>0:
-                    grid[i][j]+= min(grid[i-1][j], grid[i][j-1]) 
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if i ==0 and j==0:
+                   continue
+                elif i==0:
+                   grid[i][j] = grid[i][j] + grid[i][j-1]
+                elif j==0:
+                   grid[i][j] = grid[i][j] + grid[i-1][j]
                 else:
-                    grid[0][0] = grid[0][0]
-        return grid[numrows-1][numcols-1]
+                   grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1])
+        return grid[m-1][n-1]
+
+    
+class Solution2(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        f = [[0]*n]*m
+        for i in range(m):
+            for j in range(n):
+                if i ==0 and j==0:
+                   f[i][j] = grid[i][j]
+                elif i==0:
+                   f[i][j] = grid[i][j] + f[i][j-1]
+                elif j==0:
+                   f[i][j] = grid[i][j] + f[i-1][j]
+                else:
+                   f[i][j] = grid[i][j] + min(f[i-1][j], f[i][j-1])
+        return f[m-1][n-1]
