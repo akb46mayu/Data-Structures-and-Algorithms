0a6821a34b0a0727ce3206a4f9a45242aacd8f11
akb46mayu <bofansunrise@gmail.com>
2017-01-31 17:31:41 -0500

    Create le151_reverse_words_inastring.py

---
+++ String/le151_reverse_words_inastring.py
@@ -0,0 +1,18 @@
+"""
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Update (2015-02-12):
+For C programmers: Try to solve it in-place in O(1) space.
+"""
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ' '.join(reversed(s.split()))
