4565d0a36853170732a8282956790692a4c27bba
akb46mayu <bofansunrise@gmail.com>
2017-02-02 16:38:32 -0500

    Create le328_oddEvenLinkedList.py

---
+++ LinkedList/le328_oddEvenLinkedList.py
@@ -0,0 +1,40 @@
+"""
+Given a singly linked list, group all odd nodes together followed by the even nodes. 
+Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example:
+Given 1->2->3->4->5->NULL,
+return 1->3->5->2->4->NULL.
+
+Note:
+The relative order inside both the even and odd groups should remain as it was in the input. 
+The first node is considered odd, the second node even and so on ...
+
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        while not head:
+            return head
+        p, q = head, head
+        while q:
+            q = q.next
+            if not q or not q.next: break
+            np, nq = p.next, q.next
+            q.next = nq.next
+            p.next, nq.next = nq, np
+            p = p.next
+        return head
