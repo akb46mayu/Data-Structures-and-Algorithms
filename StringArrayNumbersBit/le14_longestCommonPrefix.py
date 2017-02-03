1ae4b365860377f0d90c0786e822d16d51e8131b
akb46mayu <bofansunrise@gmail.com>
2017-02-03 12:28:43 -0500

    Create le14_longestCommonPrefix.py

---
+++ StringArrayNumbersBit/le14_longestCommonPrefix.py
@@ -0,0 +1,20 @@
+"""
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+
+"""
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if  not strs :
+            return ''
+        for i in range(len(strs[0])):    
+            for string in strs[1:]:
+                if i >= len(string) or string[i]!=strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
