8ca552d59d40eaa99e9112efc030fc32da0cc765
akb46mayu <bofansunrise@gmail.com>
2017-02-03 11:04:55 -0500

    Create le338_countingBits.py

---
+++ StringArrayNumbersBit/le338_countingBits.py
@@ -0,0 +1,30 @@
+"""
+
+
+Given a non negative integer number num. 
+For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). 
+But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+Hint:
+
+You should make use of what you have produced already.Show More Hint 
+
+"""
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        out = [0]
+        for i in range(1, num+1):
+            out.append((i&1) + out[i>>1])
+        return out
