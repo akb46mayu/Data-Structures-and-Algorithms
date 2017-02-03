6c4068985297c4d89c3c36e24f8bac788104ad3b
akb46mayu <bofansunrise@gmail.com>
2017-02-03 17:08:08 -0500

    Create le171_excelSheetNumber.py

---
+++ String/le171_excelSheetNumber.py
@@ -0,0 +1,28 @@
+"""
+
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+
+"""
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        out = 0
+        for i in s:
+            out = out*26 + ord(i) - ord('A') + 1
+        return out
