18c2084c182ce4b44b84b25b2e9ca4cd4dbc9b18
akb46mayu <bofansunrise@gmail.com>
2017-02-11 17:03:40 -0500

    Create le150_evaluateReversePolishNotation.py

---
+++ Math/le150_evaluateReversePolishNotation.py
@@ -0,0 +1,39 @@
+"""
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
+"""
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        operators = ['+','-','*','/']
+        stack = []
+        for x in tokens:
+            if x not in operators:
+                stack.append(int(x))
+            else:
+                op1 = stack.pop()
+                op2 = stack.pop()
+                ele = self.operations(op1, op2, x)
+                stack.append(ele)
+        return stack[0]
+    def operations(self, op1, op2, operand):
+
+        if operand == '+':
+            out = op1 + op2
+        elif operand == '-':
+            out = op2 - op1
+        elif operand == '*':
+            out = op1 * op2
+        else:
+            out = int((1.0*op2/op1))
+        return out
