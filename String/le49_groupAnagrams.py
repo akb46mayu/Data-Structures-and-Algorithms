cc9bd6e3e0f6d7993b75b94fb06579f2c713c743
akb46mayu <bofansunrise@gmail.com>
2017-02-13 10:39:52 -0500

    Create le49_groupAnagrams.py

---
+++ String/le49_groupAnagrams.py
@@ -0,0 +1,31 @@
+"""
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case.
+
+
+
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        out = []
+        dict = {}
+        for word in strs:
+            sortedword = ''.join(sorted(word))
+            dict[sortedword] = [word] if sortedword not in dict else dict[sortedword]+ [word]
+        for k in dict:
+            out.append(dict[k])
+        return out
