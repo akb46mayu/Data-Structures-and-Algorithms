4bc84d21aa594085a870671928a905269d517c2d
akb46mayu <bofansunrise@gmail.com>
2017-02-16 12:40:56 -0500

    Update le17_letterCombinationOfANumber.py

--- StringArrayNumbersBit/le17_letterCombinationOfANumber.py
+++ StringArrayNumbersBit/le17_letterCombinationOfANumber.py
@@ -30,3 +30,33 @@ class Solution(object):
                        tmp.append(ch)
             out = tmp  # or use  out =copy.copy(tmp)
         return out
+
+
+class Solution2(object): #dfs
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dict = {'2':['a','b','c'],
+                '3':['d','e','f'],
+                '4':['g','h','i'],
+                '5':['j','k','l'],
+                '6':['m','n','o'],
+                '7':['p','q','r','s'],
+                '8':['t','u','v'],
+                '9':['w','x','y','z']
+                }
+        if len(digits) == 0:
+            return []
+        out = []
+        self.dfs(0, '', out, digits, dict)
+        return out
+        
+    def dfs(self, layernum, tempstr, out, digits, dict):
+        digitslen = len(digits)
+        if layernum == digitslen:
+            out.append(tempstr)
+            return 
+        for ch in dict[digits[layernum]]:
+            self.dfs( layernum+1, tempstr+ch, out, digits, dict)
