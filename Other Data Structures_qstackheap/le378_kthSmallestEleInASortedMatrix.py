ea05e26537ed573bb1c9d84ee9a8d5a179777d67
akb46mayu <bofansunrise@gmail.com>
2017-02-20 16:30:09 -0500

    Create le378_kthSmallestEleInASortedMatrix.py

---
+++ Other Data Structures_qstackheap/le378_kthSmallestEleInASortedMatrix.py
@@ -0,0 +1,42 @@
+"""
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, 
+find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ n2.
+
+"""
+
+from heapq import heappush, heappop
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        myheapq = []
+        def push(i,j):
+            if i < len(matrix) and j < len(matrix[0]):
+                heappush(myheapq, [matrix[i][j], i, j])
+        push(0,0)
+        while k > 0 and myheapq:
+            cursmallest, i, j,  = heappop(myheapq)
+            push(i, j+1)
+            if j == 0:
+                push(i+1, 0)
+            k = k - 1
+        return cursmallest
