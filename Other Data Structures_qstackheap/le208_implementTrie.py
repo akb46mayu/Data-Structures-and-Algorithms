d0cbaaac08e0c57b46f4666c667b3975fb1b1de2
akb46mayu <bofansunrise@gmail.com>
2017-02-28 15:17:36 -0500

    Create le208_implementTrie.py

---
+++ Other Data Structures_qstackheap/le208_implementTrie.py
@@ -0,0 +1,71 @@
+"""
+
+Implement a trie with insert, search, and startsWith methods.
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+
+"""
+
+
+
+class TrieNode:
+    def __init__(self):
+        self.childs = {}
+        self.isWord = False
+
+class Trie(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for letter in word:
+           child = node.childs.get(letter)
+           if child is None:
+               child = TrieNode()
+               node.childs[letter] = child
+           node = child
+        child.isWord = True
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for letter in word:
+            if not node.childs.get(letter):
+                return False
+            node = node.childs[letter]
+        return node.isWord
+            
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for letter in prefix:
+            if not node.childs.get(letter):
+                return False
+            node = node.childs[letter]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
