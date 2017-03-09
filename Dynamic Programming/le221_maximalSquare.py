e82d5a5e409f27d833e275c66c6c05942955ee33
akb46mayu <bofansunrise@gmail.com>
2017-03-09 16:08:11 -0500

    Update le221_maximalSquare.py

--- Dynamic Programming/le221_maximalSquare.py
+++ Dynamic Programming/le221_maximalSquare.py
@@ -13,7 +13,28 @@ Return 4.
 
 """
 
-class Solution(object):
+class Solution(object): # save space
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        res = [[0] * n for j in range(2)]
+        out = 0
+        for i in range(m):
+            for j in range(n):
+                 if (i == 0 and j == 0) or (i == 0) or (j == 0):
+                     res[i%2][j] = int(matrix[i][j])
+                 else:
+                     res[i%2][j] = min(res[(i-1)%2][j], res[i%2][j-1], res[(i-1)%2][j-1]) + 1 \
+                                 if int(matrix[i][j]) ==  1 else 0
+                 out = max(out, res[i%2][j])
+        return out**2
+    
+class Solution2(object):
     def maximalSquare(self, matrix):
         """
         :type matrix: List[List[str]]
