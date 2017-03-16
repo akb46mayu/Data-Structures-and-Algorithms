700c071c85fce9c8e3f22c3dbdba8e8a95e4c61e
akb46mayu <bofansunrise@gmail.com>
2017-03-15 22:05:27 -0400

    Update le162_find_peak_element.py

--- Binary Search and RSA/le162_find_peak_element.py
+++ Binary Search and RSA/le162_find_peak_element.py
@@ -1,3 +1,17 @@
+"""
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+
+click to show spoilers.
+"""
+
 class Solution(object): # I prefer to this solution (no duplicates)
     def findPeakElement(self, nums):
         """
@@ -36,3 +50,29 @@ class Solution2(object):
             else:
                 start = mid + 1  # if not use this (say start = mid), when nums=[1,2], it will loop forever
         return start
+    
+class Solution3(object): # nine chapter's template
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        n = len(nums)
+        
+        left, right = 0, n-1
+        while left + 1 < right:
+            mid = left + (right - left)/2
+            if (mid == 0 or nums[mid] > nums[mid+1]) and (mid == n-1 or nums[mid] > nums[mid-1]): # when mid =0 or n-1
+                return mid
+            elif (nums[mid] < nums[mid-1]): # in fact mid!=0
+                right = mid
+            elif (nums[mid] < nums[mid+1]): #mid + 1!= n-1
+                left = mid
+            else:
+                right = mid
+        if nums[left] > nums[right]:
+            return left
+        else:
+            return right
