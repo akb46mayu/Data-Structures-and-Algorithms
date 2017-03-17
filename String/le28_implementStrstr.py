702f94307ec5d80136d6e8641aac0658ff6f2839
akb46mayu <bofansunrise@gmail.com>
2017-03-17 17:14:16 -0400

    Update and rename le28_implement_strstr.py to le28_implementStrstr.py

---
+++ String/le28_implementStrstr.py
@@ -0,0 +1,21 @@
+"""
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack is None or needle is None:  # i suggest to add this. 
+            return -1
+        tl = len(needle)
+        n = len(haystack)
+        for i in range(n-tl+1): # in the program itself it automatically include the case when src = '' and target = ''
+            if haystack[i:i+tl] == needle:
+                return i
+        return -1
