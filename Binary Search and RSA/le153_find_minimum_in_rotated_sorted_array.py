9e8cd36a2be69277d4a5a59b5ec09e98375bd4d7
akb46mayu <bofansunrise@gmail.com>
2017-03-18 12:12:14 -0400

    Update le153_find_minimum_in_rotated_sorted_array.py

--- Binary Search and RSA/le153_find_minimum_in_rotated_sorted_array.py
+++ Binary Search and RSA/le153_find_minimum_in_rotated_sorted_array.py
@@ -1,3 +1,20 @@
+"""
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+ Notice
+
+You may assume no duplicate exists in the array.
+
+Have you met this question in a real interview? Yes
+Example
+Given [4, 5, 6, 7, 0, 1, 2] return 0
+
+"""
+
 class Solution(object):
     def findMin(self, nums):
         """
@@ -13,3 +30,26 @@ class Solution(object):
             else:
                 start = mid
         return min(nums[start], nums[end])
+
+
+    
+    
+    
+class Solution2: # easily understanding verison
+    # @param nums: a rotated sorted array
+    # @return: the minimum number in the array
+    def findMin(self, nums):
+        # write your code here
+        if not nums:
+            return -1
+        
+        left, right = 0, len(nums)-1
+        while left + 1 < right:
+            mid = (left + right)/2
+            if nums[mid] > nums[right] and nums[mid] > nums[left]:
+                left = mid
+            elif nums[mid] < nums[right] and nums[mid] < nums[left]:
+                right = mid
+            else:
+                right = mid
+        return min(nums[left], nums[right])
