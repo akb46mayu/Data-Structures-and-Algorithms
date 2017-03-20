c27b2277d29bbb356e6d63972391f0700cc20b57
akb46mayu <bofansunrise@gmail.com>
2017-03-20 12:09:16 -0400

    Create le94_binaryTreeInorderTraversal.py

---
+++ BinaryTree and Divide and Conquer/le94_binaryTreeInorderTraversal.py
@@ -0,0 +1,41 @@
+"""
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        ans = []
+        self.inorderHelper(root, ans)
+        return ans
+        
+        
+    def inorderHelper(self, root, ans):
+        if not root:
+            return
+        self.inorderHelper(root.left, ans)
+        ans.append(root.val)
+        self.inorderHelper(root.right, ans)
