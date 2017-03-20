8c152825004237cc663260a67c875aaef56c6dbb
akb46mayu <bofansunrise@gmail.com>
2017-03-20 16:40:20 -0400

    Update le144_binarytreePreorderTraversal.py

--- BinaryTree and Divide and Conquer/le144_binarytreePreorderTraversal.py
+++ BinaryTree and Divide and Conquer/le144_binarytreePreorderTraversal.py
@@ -5,7 +5,7 @@
 #         self.left = None
 #         self.right = None
 
-class Solution(object): # use recursion
+class Solution(object): #  recursion and traverse
     def preorderTraversal(self, root):
         """ 
         :type root: TreeNode
@@ -41,3 +41,19 @@ class Solution2(object): # use iteration (non-recursion) and stack
             if cur.left:
                 stack.append(cur.left)
         return results
+    
+class Solution3: # recursion- divide and conquer
+    """
+    @param root: The root of binary tree.
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        # write your code here
+        
+        result = []
+        if not root:
+            return result
+        left = self.preorderTraversal(root.left)
+        right = self.preorderTraversal(root.right)
+        result = [root.val] + left + right
+        return result
