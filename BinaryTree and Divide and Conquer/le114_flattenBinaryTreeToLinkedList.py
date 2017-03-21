212e081525790bab7ca1e1930fc1470cf088adde
akb46mayu <bofansunrise@gmail.com>
2017-03-20 23:30:00 -0400

    Create le114_flattenBinaryTreeToLinkedList.py

---
+++ BinaryTree and Divide and Conquer/le114_flattenBinaryTreeToLinkedList.py
@@ -0,0 +1,56 @@
+"""
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        self.lastNode = None
+        self.dfsHelper(root)
+
+        
+    def dfsHelper(self, root):
+        if not root:
+            return
+        if self.lastNode:
+            self.lastNode.left = None
+            self.lastNode.right = root
+        self.lastNode = root
+        right = root.right
+        self.dfsHelper(root.left)
+        self.dfsHelper(right)
