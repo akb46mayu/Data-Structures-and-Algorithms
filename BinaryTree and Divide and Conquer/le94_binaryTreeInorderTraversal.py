51a2ebb3b12f0df220ea66633dabbf6da918e4c2
akb46mayu <bofansunrise@gmail.com>
2017-03-23 17:02:44 -0400

    Update le94_binaryTreeInorderTraversal.py

--- BinaryTree and Divide and Conquer/le94_binaryTreeInorderTraversal.py
+++ BinaryTree and Divide and Conquer/le94_binaryTreeInorderTraversal.py
@@ -68,3 +68,28 @@ class Solution2(object): # non-recursion
                         break
         return ans      
       
+### JAVA---------------------
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        TreeNode popnode = root;
+        while (popnode!=null || !stack.empty()){
+          while (popnode!=null){
+            stack.add(popnode);
+            popnode = popnode.left;
+          }
+          popnode = stack.peek();  // record the referene before poping
+          stack.pop();
+          result.add(popnode.val);
+          popnode = popnode.right;
+          
+        }
+        return result;
+    }
+}
