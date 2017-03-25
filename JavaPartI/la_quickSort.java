1d486507e656b25459cbd8d0b3112af063f5a4f0
akb46mayu <bofansunrise@gmail.com>
2017-03-25 17:04:44 -0400

    Create la_quickSort.java

---
+++ JavaPartI/la_quickSort.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+public class QuickSort {
+	public int[] quickSort(int[] array) {
+	    // Write your solution here
+		if (array == null){
+			return array;
+		}
+		quickSort(array, 0, array.length-1);
+	    return array;
+	}
+	
+	public void quickSort(int[] array, int left, int right){
+		if (left >= right){
+			return;
+		}
+		int pivotidx = partition(array, left, right);
+		quickSort(array, left, pivotidx-1);
+		quickSort(array, pivotidx+1, right);
+		
+	}
+	private void swap(int[] array, int left, int right){
+		int temp = array[left];
+		array[left] = array[right];
+		array[right] = temp;
+	}
+	private int partition(int[] array, int left, int right){
+	    int pivotidx = pivotidxFunc(left, right);
+	    int pivotval = array[pivotidx];
+	    swap(array, right, pivotidx);
+	     // i is the idx all smaller values are before the idx
+	    int newi = left;
+	    for(int i=left; i<= right; i++){
+	    	if (array[i] < pivotval){
+	    		swap(array, newi, i);
+	    		newi++;
+	    	}
+	    }
+	    swap(array, newi, right);
+	    return newi;
+	}
+	private int pivotidxFunc(int left, int right){
+		return left + (int)(Math.random()*(right - left + 1)); //check the meaning of this
+	}
+	public static void main(String[] args){
+		QuickSort qsort = new QuickSort();
+		int[] array = {4,2,1,6,3,5};
+		qsort.quickSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+	
+}
