d8496d2612fea24c7ea333dc31854cdb5aaf494c
akb46mayu <bofansunrise@gmail.com>
2017-03-26 15:16:39 -0400

    Update la_moveZerosToTheEnd.java

--- JavaPartI/la_moveZerosToTheEnd.java
+++ JavaPartI/la_moveZerosToTheEnd.java
@@ -2,31 +2,31 @@
 
 import java.util.Arrays;
 public class move0 {
-	  public int[] moveZero(int[] array) {
-		    // Write your solution here.
-		    if (array.length == 0){
-		      return array;
-		    }
-		    int zero = array.length - 1, i = 0;
-		    while (i <= zero){
-		      if (array[i] == 0){
-		        swap(array, i, zero--);
-		      }
-		      i++;
-		    }
-		    return array;
-		  }  
-		  
-		  private void swap(int[] array, int left, int right){
-		    int temp = array[left];
-		    array[left] = array[right];
-		    array[right] = temp;
-		  }
-	  public static void main(String[] args){
-		  move0 a = new move0();
-		  int[] array = {2,4,1,0,0,7,3};
-		  a.moveZero(array);
-		  System.out.println(Arrays.toString(array));
-	  }
+	public int[] moveZero(int[] array) {
+	    // Write your solution here.
+	    if (array.length == 0){
+	      return array;
+	    }
+	    int zero = array.length - 1, i = 0;
+	    while (i <= zero){
+	      if (array[i] == 0){
+		swap(array, i, zero--);
+	      }
+	      i++;
+	    }
+	    return array;
+	  }  
+
+	private void swap(int[] array, int left, int right){
+	int temp = array[left];
+	array[left] = array[right];
+	array[right] = temp;
+	}
+	public static void main(String[] args){
+	  move0 a = new move0();
+	  int[] array = {2,4,1,0,0,7,3};
+	  a.moveZero(array);
+	  System.out.println(Arrays.toString(array));
+	}
 
 }
