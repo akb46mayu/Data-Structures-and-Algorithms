56bface9b61e66e5a5df7a971e365272e75ccfaf
akb46mayu <bofansunrise@gmail.com>
2017-03-27 16:09:25 -0400

    Create la_firstOccurrence.java

---
+++ JavaPartI/la_firstOccurrence.java
@@ -0,0 +1,28 @@
+//
+public class Solution {
+  public int firstOccur(int[] array, int target) {
+    // Write your solution here
+    
+    if (array == null){
+      return -1;
+    }
+    int left = 0, right = array.length - 1;
+    while (left <= right){
+      int mid = left + (right - left)/2;
+      if (array[mid] == target){
+         if (mid == 0 || array[mid-1] < array[mid]){
+           return mid;
+         }else{
+           right = mid - 1;
+         }
+      } else if (array[mid] < target){
+          left = mid + 1;
+      } else{
+          right = mid - 1;
+      }
+         
+    }
+    
+    return -1;
+  }
+}
