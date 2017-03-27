014e0316b59e39b8f2d5e885c5958929eaeab4ad
akb46mayu <bofansunrise@gmail.com>
2017-03-27 11:31:55 -0400

    Create la_fibonacci.java

---
+++ JavaPartI/la_fibonacci.java
@@ -0,0 +1,23 @@
+
+
+
+// TLE solution 
+
+public class Solution {
+  public long fibonacci(int K) {
+    // Write your solution here
+    if (K < 0) {
+      return 0;
+      
+    }
+    
+    if (K == 0){
+      return 0;
+    }
+    if (K == 1){
+      return 1;
+      
+    }
+    return fibonacci(K-1) + fibonacci(K-2);
+  }
+}
