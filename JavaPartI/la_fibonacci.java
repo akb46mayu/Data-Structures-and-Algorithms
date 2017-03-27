4da4591b3e375fbf43a41488c44a04d7b73a535e
akb46mayu <bofansunrise@gmail.com>
2017-03-27 12:09:12 -0400

    Update la_fibonacci.java

--- JavaPartI/la_fibonacci.java
+++ JavaPartI/la_fibonacci.java
@@ -38,3 +38,23 @@ public class Solution {
     return f[K];
   }
 }
+// O(1) space
+
+public class Solution {
+  public long fibonacci(int K) {
+    // Write your solution here
+    if (K <= 0) {
+      return 0;
+    }
+    
+    if (K == 1){
+      return 1;
+    }
+    long[] f = new long[2];
+    f[1] = 1;
+    for (int i = 2; i <= K; i++){
+      f[i%2] = f[(i-1)%2] + f[(i-2)%2];
+    }
+    return f[K%2];
+  }
+}
