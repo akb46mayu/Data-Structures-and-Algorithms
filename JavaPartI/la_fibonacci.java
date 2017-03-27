0a8a972f6374682e6d0a64e232d20058f1cf4cff
akb46mayu <bofansunrise@gmail.com>
2017-03-27 11:33:48 -0400

    Update la_fibonacci.java

--- JavaPartI/la_fibonacci.java
+++ JavaPartI/la_fibonacci.java
@@ -8,15 +8,12 @@ public class Solution {
     // Write your solution here
     if (K < 0) {
       return 0;
-      
     }
-    
     if (K == 0){
       return 0;
     }
     if (K == 1){
       return 1;
-      
     }
     return fibonacci(K-1) + fibonacci(K-2);
   }
