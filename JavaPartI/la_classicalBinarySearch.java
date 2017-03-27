4ce28dc9cd3306fe35d2ff8597b8e93e3cc1755a
akb46mayu <bofansunrise@gmail.com>
2017-03-27 12:33:10 -0400

    Create la_classicalBinarySearch.java

---
+++ JavaPartI/la_classicalBinarySearch.java
@@ -0,0 +1,23 @@
+//
+
+public class Solution {
+  public int binarySearch(int[] array, int target) {
+    // Write your solution here.
+    if (array == null){
+      return -1;
+    }
+    int left = 0, right = array.length - 1;
+    while (left <= right){
+      int mid = left + (right - left)/2;
+      if (target == array[mid]){
+        return mid;
+      }else if (target < array[mid]){
+        right = mid - 1;
+      }else{
+        left = mid + 1;
+      }
+    }
+    
+    return -1;
+  }
+}
