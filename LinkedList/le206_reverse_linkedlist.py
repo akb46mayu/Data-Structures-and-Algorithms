45a0fe89428a2e859d844aec7f25088f3d8ab4cd
akb46mayu <bofansunrise@gmail.com>
2017-04-01 12:11:22 -0400

    Update le206_reverse_linkedlist.py

--- LinkedList/le206_reverse_linkedlist.py
+++ LinkedList/le206_reverse_linkedlist.py
@@ -7,16 +7,18 @@ Reverse a singly linked list.
 #         self.val = x
 #         self.next = None
 
+    
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        cur = head  # this is more clear 
         prev = None
-        while head:
-            cur = head
-            head = head.next
+        while cur!= None:
+            temp = cur.next
             cur.next = prev
             prev = cur
+            cur = temp
         return prev
