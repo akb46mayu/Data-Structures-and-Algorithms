4c99b53ef9913a0e4e9b0a4eb8ebdbf8760135b6
akb46mayu <bofansunrise@gmail.com>
2017-04-01 12:54:52 -0400

    Create la_checkIfLinkedListHasACycle.java

---
+++ JavaPartI/la_checkIfLinkedListHasACycle.java
@@ -0,0 +1,24 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    // write your solution here
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
