2879dd9a805db29608bff3b2a44c76cc1069e547
akb46mayu <bofansunrise@gmail.com>
2017-04-01 17:24:46 -0400

    Create la_mergeTwoSortedLinkedLists.java

---
+++ JavaPartI/la_mergeTwoSortedLinkedLists.java
@@ -0,0 +1,33 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode merge(ListNode one, ListNode two) {
+    // write your solution here
+    ListNode dummy = new ListNode(0);
+    ListNode cur = dummy;
+    while (one!=null && two != null) {
+      if (one.value <= two.value) {
+         cur.next = one;
+         one = one.next;
+      } else {
+         cur.next = two;
+         two = two.next;
+      }
+      cur = cur.next;
+    }
+    if (one!=null) {
+      cur.next = one;
+    } else {
+      cur.next = two;
+    }
+    return dummy.next;
+  }
+}
