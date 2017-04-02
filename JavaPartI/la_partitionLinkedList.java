7d107ca56882c556572665d437f5f997ebcacb5b
akb46mayu <bofansunrise@gmail.com>
2017-04-02 15:46:25 -0400

    Create la_partitionLinkedList.java

---
+++ JavaPartI/la_partitionLinkedList.java
@@ -0,0 +1,35 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode partition(ListNode head, int target) {
+    // write your solution here
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode dummysmall = new ListNode(0);
+    ListNode dummylarge = new ListNode(0);
+    
+    ListNode curs = dummysmall, curl = dummylarge;
+    while (head!=null) {
+      if (head.value < target) {
+        curs.next = head;
+        curs = curs.next;
+      } else {
+        curl.next = head;
+        curl = curl.next;
+      }
+      head = head.next;
+    }
+    curs.next = dummylarge.next;
+    curl.next = null;
+    return dummysmall.next;
+  }
+}
