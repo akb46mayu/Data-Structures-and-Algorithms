0b2dfc553eb8adaeb43d17300ae88c8e35a2130c
akb46mayu <bofansunrise@gmail.com>
2017-04-12 12:08:17 -0400

    Create la_preOrderTraversalOfBinaryTree.java

---
+++ JavaPartI/la_preOrderTraversalOfBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public List<Integer> preOrder(TreeNode root) {
+    // Write your solution here.
+    List<Integer> result = new ArrayList<Integer>();
+    if (root == null){
+      return result;
+    }
+    
+    
+    List<Integer> left = preOrder(root.left);
+    List<Integer> right = preOrder(root.right);
+    result.add(root.key);
+    result.addAll(left);
+    result.addAll(right);
+    return result;
+    
+  }
+}
