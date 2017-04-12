19fdad9e16a9fd202aacacbbb3c551290438448f
akb46mayu <bofansunrise@gmail.com>
2017-04-12 18:09:38 -0400

    Update le94_binaryTreeInorderTraversal.java

--- BinaryTree and Divide and Conquer/le94_binaryTreeInorderTraversal.java
+++ BinaryTree and Divide and Conquer/le94_binaryTreeInorderTraversal.java
@@ -1,1 +1,26 @@
 
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        TreeNode popnode = root;
+        while (popnode!=null || !stack.empty()){
+          while (popnode!=null){
+            stack.add(popnode);
+            popnode = popnode.left;
+          }
+          popnode = stack.peek();  // record the referene before poping
+          stack.pop();
+          result.add(popnode.val);
+          popnode = popnode.right;
+          
+        }
+        return result;
+    }
+}
