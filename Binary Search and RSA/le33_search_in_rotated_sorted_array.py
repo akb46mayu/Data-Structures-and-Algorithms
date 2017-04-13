11d1f74553c15d64684c0808d0d306d2e54c45b1
akb46mayu <bofansunrise@gmail.com>
2017-04-13 16:45:28 -0400

    Update le33_search_in_rotated_sorted_array.py

--- Binary Search and RSA/le33_search_in_rotated_sorted_array.py
+++ Binary Search and RSA/le33_search_in_rotated_sorted_array.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):   # can also use the classical binary search without post processing, see java
     def search(self, nums, target):
         """
         :type nums: List[int]
