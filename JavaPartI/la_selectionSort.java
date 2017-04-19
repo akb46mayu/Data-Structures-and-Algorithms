e8fdd3ba6834c2bcc3623053f8424e5eaa110ff7
akb46mayu <bofansunrise@gmail.com>
2017-04-18 20:45:34 -0400

    Update la_selectionSort.java

--- JavaPartI/la_selectionSort.java
+++ JavaPartI/la_selectionSort.java
@@ -24,3 +24,33 @@ public class Solution {
     return array;
   }
 }
+
+
+
+public class Solution2 {
+  public int[] solve(int[] array) {
+    // Write your solution here.
+    
+    if (array == null || array.length == 0) {
+      return array;
+    }
+    int n = array.length;
+    for (int i = 0; i < n - 1; i++) {
+      int minidx = i;
+      for (int j = i + 1; j < n; j ++) {
+        if (array[minidx] > array[j]) {
+          swap(array, i, j);
+        }
+      }
+    }
+    
+    return array;
+  }
+  
+  private void swap(int[] array, int left, int right) {
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+  }
+  
+}
