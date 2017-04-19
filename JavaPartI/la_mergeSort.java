5b5601250a271c6dc889ea7901c0550f251d363b
akb46mayu <bofansunrise@gmail.com>
2017-04-18 22:00:53 -0400

    Update la_mergeSort.java

--- JavaPartI/la_mergeSort.java
+++ JavaPartI/la_mergeSort.java
@@ -41,3 +41,48 @@ public class Solution {
   
   }
 }
+
+
+
+public class Solution2 {
+   	  public int[] mergeSort(int[] array) {
+		    // Write your solution here.
+		    if (array == null || array.length == 0) {
+		      return array;
+		    }
+		    return mergeSortHelper(array, 0, array.length - 1);
+		  }
+		  
+		  private int[] mergeSortHelper(int[] array, int left, int right) {
+		    if (left == right) {
+		      int[] out = new int[1];
+		      out[0] = array[left];
+		      return out;
+		    }
+		    int mid = left + (right - left)/2;
+		    int[] leftpart = mergeSortHelper(array, left, mid);
+		    int[] rightpart = mergeSortHelper(array, mid + 1, right);
+		    return merge(leftpart, rightpart);
+		  }
+		  
+		  private int[] merge(int[] leftpart, int[] rightpart) {
+		    int n1 = leftpart.length, n2 = rightpart.length;
+		    int[] out = new int[n1 + n2];
+		    int i = 0, j = 0, k = 0;
+		    while (i < n1 && j < n2) {
+		      if (leftpart[i] < rightpart[j]) {
+		        out[k++] = leftpart[i++];
+		      } else {
+		        out[k++] = rightpart[j++];
+		      }
+		    }
+		    
+		    while (i < n1) {
+		      out[k++] = leftpart[i++];
+		    }
+		    while (j < n2) {
+		      out[k++] = rightpart[j++];
+		    }
+		    return out;
+		  }
+}
