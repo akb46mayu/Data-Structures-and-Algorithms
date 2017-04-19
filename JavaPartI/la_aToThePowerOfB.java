d43526aadd4e1ae66b6e6a3d40e0175cb859fed1
akb46mayu <bofansunrise@gmail.com>
2017-04-19 14:44:30 -0400

    Update la_aToThePowerOfB.java

--- JavaPartI/la_aToThePowerOfB.java
+++ JavaPartI/la_aToThePowerOfB.java
@@ -13,3 +13,22 @@ public class Solution {
     return b % 2 == 0 ? half * half: a * half * half;
   }
 }
+
+
+public class Solution { // different coding style, 
+  public long power(int a, int b) {
+    // Write your solution here
+    if (b == 0) {
+      return 1;
+    }
+    if (b == 1) { // corner case is easier to remember
+      return a;
+    }
+    long temp = power(a, b / 2);
+    if (b % 2 == 0) {
+      return temp * temp;
+    } else {
+      return (temp * temp) * a;
+    }
+  }
+}
