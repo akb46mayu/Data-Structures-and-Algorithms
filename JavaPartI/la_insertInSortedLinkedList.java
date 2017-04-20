c144252cc33f796ad2fbef80430dcfb74a794b87
akb46mayu <bofansunrise@gmail.com>
2017-04-19 23:45:19 -0400

    Update la_insertInSortedLinkedList.java

--- JavaPartI/la_insertInSortedLinkedList.java
+++ JavaPartI/la_insertInSortedLinkedList.java
@@ -8,6 +8,34 @@
  *   }
  * }
  */
+
+public class Solution {  //different logic
+  public ListNode insert(ListNode head, int value) {
+    // write your solution here
+    ListNode temp = new ListNode(value);
+    if (head == null || value <= head.value) {
+      temp.next = head;
+      return temp;
+    }
+    ListNode prev = head, cur = head.next;
+    
+    while (prev.next != null) {
+      if (temp.value <= cur.value) {
+        prev.next = temp;
+        temp.next = cur;
+        return head;
+      }
+      prev = prev.next;
+      cur = cur.next;
+    }
+    
+    if (temp.value > prev.value) {
+      prev.next = temp;
+    }
+    return head;
+  }
+}
+/////////////////////////////
 public class Solution {
   public ListNode insert(ListNode head, int value) {
     // write your solution here
