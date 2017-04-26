67e9b235431cb89927dc0241a38b795c64b7a84e
akb46mayu <bofansunrise@gmail.com>
2017-04-25 20:33:01 -0400

    Update la_commonNumberOfTwoSortedArrays.java

--- JavaPartI/la_commonNumberOfTwoSortedArrays.java
+++ JavaPartI/la_commonNumberOfTwoSortedArrays.java
@@ -18,3 +18,41 @@ public class Solution {
     return result;
   }
 }
+/////
+
+public class Solution {
+  public List<Integer> common(List<Integer> A, List<Integer> B) {
+    // Write your solution here.
+    List<Integer> result = new ArrayList<Integer>();
+    HashMap<Integer, Integer> countA = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> countB = new HashMap<Integer, Integer>();
+    int a = A.size(), b = B.size();
+    for (int i = 0; i < a; i++) {
+      Integer temp = countA.get(A.get(i));
+      if (temp == null) {
+        countA.put(A.get(i), 1);
+      } else {
+        countA.put(A.get(i), temp + 1);
+      }
+    }
+    //
+    for (int i = 0; i < b; i++) {
+      Integer temp = countB.get(B.get(i));
+      if (temp == null) {
+        countB.put(B.get(i), 1);
+      } else {
+        countB.put(B.get(i), temp + 1);
+      }
+    }
+    //
+    for(Map.Entry<Integer, Integer> entry : countA.entrySet()) {
+      if (countB.containsKey(entry.getKey())) {
+        int lenInt = Math.min(countB.get(entry.getKey()), entry.getValue());
+        for (int i = 0; i < lenInt; i++) {
+          result.add(entry.getKey());
+        }
+      }
+    }
+    return result;
+  }
+}
