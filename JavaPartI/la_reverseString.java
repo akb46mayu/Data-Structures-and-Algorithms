d7a747c08f8a3352540df05528c4876a572db8cd
akb46mayu <bofansunrise@gmail.com>
2017-04-26 16:16:13 -0400

    Update la_reverseString.java

--- JavaPartI/la_reverseString.java
+++ JavaPartI/la_reverseString.java
@@ -17,3 +17,28 @@ public class Solution {
     arr[right] = temp;
   }
 }
+//// method 2, recursion
+
+public class Solution {
+  public String reverse(String input) {
+    // Write your solution here.
+    if (input == null || input.length() == 0) {
+      return input;
+    }
+    char[] arr = input.toCharArray(); 
+    reverseHelper(arr, 0, arr.length - 1);
+    return new String(arr);
+  }
+  private void reverseHelper(char[] arr, int left, int right) {
+    if (left >= right) {
+      return;
+    }
+    swap(arr, left, right);
+    reverseHelper(arr, left + 1, right - 1);
+  }
+  private void swap(char[] arr, int left, int right) {
+    char temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+  }
+}
