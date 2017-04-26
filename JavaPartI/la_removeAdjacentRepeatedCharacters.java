440ca408a9628d8eb429df1656304b6affed26ae
akb46mayu <bofansunrise@gmail.com>
2017-04-25 21:26:42 -0400

    Create la_removeAdjacentRepeatedCharacters.java

---
+++ JavaPartI/la_removeAdjacentRepeatedCharacters.java
@@ -0,0 +1,21 @@
+//
+// j: current pointer , i (inclusive) pointer about the most non duplicated region.
+public class Solution {
+  public String deDup(String input) {
+    // Write your solution here.
+    if (input == null || input.length() == 0) {
+      return input;
+    }
+    
+    char[] arr = input.toCharArray();
+    int i = 0, j = 0;
+    while (j < arr.length) {
+      if (arr[i] == arr[j]) {
+        j++;
+      } else {
+        arr[++i] = arr[j++];
+      }
+    }
+    return new String(arr, 0, i + 1);  // i + 1 total number of elements
+  } 
+}
