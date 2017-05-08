fb96c9faed53a8c4a40047a64d49f80455bc2e3c
akb46mayu <bofansunrise@gmail.com>
2017-05-08 12:16:08 -0400

    Update la_reverseLinkedlistInPairs.java

--- JavaPartI/la_reverseLinkedlistInPairs.java
+++ JavaPartI/la_reverseLinkedlistInPairs.java
@@ -8,7 +8,7 @@
  *   }
  * }
  */
-public class Solution {
+public class Solution {  // recursion
   public ListNode reverseInPairs(ListNode head) {
     // Write your solution here.
     if (head == null || head.next == null) {
@@ -18,5 +18,24 @@ public class Solution {
     head.next = reverseInPairs(head.next.next);
     newHead.next = head;
     return newHead;
+  }
+}
+
+
+
+public class Solution2 { //iteration
+  public ListNode reverseInPairs(ListNode head) {
+    // Write your solution here.
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode cur = dummy;
+    while (cur.next != null && cur.next.next != null) {
+      ListNode temp = cur.next.next;
+      cur.next.next = temp.next;
+      temp.next = cur.next;
+      cur.next = temp;
+      cur = cur.next.next;
+    }
+    return dummy.next;
   }
 }
