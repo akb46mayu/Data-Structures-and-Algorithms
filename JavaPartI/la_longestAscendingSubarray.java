448d4dd5544b6b2e909253bc445689837880fd8d
akb46mayu <bofansunrise@gmail.com>
2017-05-13 16:18:46 -0400

    Update la_longestAscendingSubarray.java

--- JavaPartI/la_longestAscendingSubarray.java
+++ JavaPartI/la_longestAscendingSubarray.java
@@ -5,6 +5,26 @@ public class Solution {
       return 0;
     }
     int n = array.length, globalMax = 1;
+    int count = 1;
+    for (int i = 1; i <= n - 1; i++) {
+      if (array[i] > array[i - 1]) {
+        count++;
+      } else {
+        count = 1;
+      }
+      globalMax = Math.max(count, globalMax);
+    }
+    return globalMax;
+  }
+}
+////
+public class Solution2 {
+  public int longest(int[] array) {
+    // Write your solution here.
+    if (array == null || array.length == 0) {
+      return 0;
+    }
+    int n = array.length, globalMax = 1;
     int[] dp = new int[n];
     dp[0] = 1;
     for (int i = 1; i <= n - 1; i++) {
