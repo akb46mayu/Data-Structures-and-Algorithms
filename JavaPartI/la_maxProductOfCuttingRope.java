ecfb2f717e98a8035962ac00bcf6b91bd135670d
akb46mayu <bofansunrise@gmail.com>
2017-05-13 17:16:25 -0400

    Create la_maxProductOfCuttingRope.java

---
+++ JavaPartI/la_maxProductOfCuttingRope.java
@@ -0,0 +1,18 @@
+/// dp - dp[i]: max product of rope with length of i, with at least one cut
+public class Solution {
+  public int maxProduct(int length) {
+    // Write your solution here.
+    if (length == 0) {
+      return 0;
+    }
+    int n = length;
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      for (int k = 1; k <= i / 2; k++) {
+        dp[i] = Math.max(dp[i], Math.max(dp[k], k) * Math.max(dp[i - k], i - k));
+      }
+    }
+    return dp[n];
+  }
+}
