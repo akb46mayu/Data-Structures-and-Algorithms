56785b2d014c58a794bb5b8da87891c33485d99e
akb46mayu <bofansunrise@gmail.com>
2017-05-14 16:34:47 -0400

    Create la_arrayHopperII.java

---
+++ JavaPartI/la_arrayHopperII.java
@@ -0,0 +1,18 @@
+public class Solution {
+  public int minJump(int[] array) {
+    // Write your solution here.
+    int n = array.length;
+    int[] dp = new int[n];
+    dp[0] = 0;
+    for (int i = 1; i <= n - 1; i++) {
+      dp[i] = -1;
+      for (int k = 0; k <= i - 1; k++) {
+        if (dp[k] != -1 && array[k] + k >= i) {
+          dp[i] = dp[k] + 1;
+          break;
+        }
+      }
+    }
+    return dp[n - 1];
+  }
+}
