eba77ea2f78d032764e2f5883d34760e3bcac3df
akb46mayu <bofansunrise@gmail.com>
2017-05-17 17:16:43 -0400

    Update la_topKFrequentWords.java

--- JavaPartI/la_topKFrequentWords.java
+++ JavaPartI/la_topKFrequentWords.java
@@ -45,9 +45,52 @@ public class Solution {
 	  }
 }
 ////////////////// the other way of defining the comparator
-class PairComparator implements Comparator<Map.Entry<String, Integer>> { 
-    	public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-            return e1.getValue().compareTo(e2.getValue());
-        }
+
+class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+  public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+    return e1.getValue().compareTo(e2.getValue());
+  }
 }
-PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<Map.Entry<String, Integer>>(k, new PairComparator());
+
+public class Solution {
+  public String[] topKFrequent(String[] combo, int k) {
+    // Write your solution here.
+    if (combo.length == 0) {
+      return combo;
+    }
+    
+    Map<String, Integer> fMap = countWord(combo);
+    
+    PriorityQueue<Map.Entry<String, Integer>> minHeap = new PriorityQueue<> (k, new MyComparator());
+    for (Map.Entry<String, Integer> entry : fMap.entrySet()) {
+      if (minHeap.size() < k) {
+        minHeap.offer(entry);
+      } else {
+        if (minHeap.peek().getValue() < entry.getValue()) {
+          minHeap.poll();
+          minHeap.offer(entry);
+        }
+      }
+    }
+    // print
+    String[] result = new String[minHeap.size()];
+    for (int i = minHeap.size() - 1; i >= 0; i--) {
+      result[i] = minHeap.poll().getKey();
+    }
+    return result;
+  }
+  
+  private Map<String, Integer> countWord(String[] comb) {
+    Map<String, Integer> freqMap = new HashMap<String, Integer>();
+    for (String str : comb) {
+      Integer temp = freqMap.get(str);
+      if (temp != null) {
+        freqMap.put(str, temp + 1);
+      } else {
+        freqMap.put(str, 1);
+      }
+    }
+    return freqMap;
+  }
+  
+}
