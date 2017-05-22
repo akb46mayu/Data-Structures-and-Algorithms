88f31f2e94bd17b96913060d8a5a46c1c0fbf5e7
akb46mayu <bofansunrise@gmail.com>
2017-05-21 21:42:14 -0400

    Update la_determineIfOneStringIsAnothersSubstring.java

--- JavaPartI/la_determineIfOneStringIsAnothersSubstring.java
+++ JavaPartI/la_determineIfOneStringIsAnothersSubstring.java
@@ -25,3 +25,38 @@ public class Solution {
     return -1;
   }
 }
+
+// way 2 of implementation
+
+
+public class Solution {
+  public int strstr(String large, String small) {
+    if (small == null || small.length() == 0) {
+      return 0;
+    } 
+    if (large == null || large.length() == 0) {
+      return -1;
+    }
+    int lenl = large.length(), lens = small.length();
+    if (lens > lenl) {
+      return - 1;
+    }
+    for (int i = 0; i <= lenl - lens; i++) {
+      if (checkStr(large, small, i)) {
+        return i;
+      }
+    }
+    return -1;
+    
+  }
+  private boolean checkStr(String large, String small, int start) {
+    int lenl = large.length(), lens = small.length();
+    for (int j = 0; j < lens; j++) {
+      if (small.charAt(j) != large.charAt(start)) {
+        return false;
+      }
+      start++;
+    }
+    return true;
+  }
+}
