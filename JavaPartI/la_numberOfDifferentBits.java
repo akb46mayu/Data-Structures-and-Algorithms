05c1d1d9320c83e756d786dca9e688ac0a3cb266
akb46mayu <bofansunrise@gmail.com>
2017-05-26 12:43:43 -0400

    Update la_numberOfDifferentBits.java

--- JavaPartI/la_numberOfDifferentBits.java
+++ JavaPartI/la_numberOfDifferentBits.java
@@ -10,3 +10,18 @@ public class Solution {
     return count;
   }
 }
+
+
+////
+public class Solution {
+  public int diffBits(int a, int b) {
+    // Write your solution here.
+    int c = a ^ b;
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+      count += (c & 1);
+      c >>= 1;
+    }
+    return count;
+  }
+}
