d4d0d1aab3819d652a796a60e007854c7212e7c7
akb46mayu <bofansunrise@gmail.com>
2017-05-25 23:06:39 -0400

    Update la_stringAbbreviationMatching.java

--- JavaPartI/la_stringAbbreviationMatching.java
+++ JavaPartI/la_stringAbbreviationMatching.java
@@ -1,3 +1,4 @@
+// recursion
 public class Solution {
   public boolean match(String input, String pattern) {
     // Write your solution here.
@@ -23,5 +24,39 @@ public class Solution {
       }
       return matchHelper(s, t, si + count, ti);
     }
+  }
+}
+
+//// iteration: firstly check alphabetic, then digit, consider length prolblems of two strings
+public class Solution2 {
+  public boolean match(String input, String pattern) {
+    // Write your solution here.
+    if (input.length() == 0 && pattern.length() == 0) {
+      return true;
+    } else if (input.length() == 0 || pattern.length() == 0) {
+      return false;
+    } 
+    int j = 0, i = 0;
+    while (j < pattern.length()) {
+      if (i >= input.length()) {
+        return false;
+      }
+      if ((pattern.charAt(j) - '0') < 0 || (pattern.charAt(j) - '0') > 9) { // it is an alphabatic 
+        if (input.charAt(i) != pattern.charAt(j)) {
+          return false;
+        }
+        i++;
+        j++;
+      } else {  // it is a digit
+        int prevsum = 0;
+        while (j < pattern.length() && (pattern.charAt(j) - '0') >= 0 && (pattern.charAt(j) - '0') <= 9) {
+            prevsum = prevsum * 10 + (pattern.charAt(j) - '0');
+            j++;
+        }
+        i += prevsum;
+      }
+    }
+    return i == input.length();
+
   }
 }
