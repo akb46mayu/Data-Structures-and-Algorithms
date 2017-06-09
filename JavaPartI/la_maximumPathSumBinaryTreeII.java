fdad7a4d2c1c2729093b428522c510c3aeecda64
akb46mayu <bofansunrise@gmail.com>
2017-06-09 11:55:20 -0400

    Create la_maximumPathSumBinaryTreeII.java

---
+++ JavaPartI/la_maximumPathSumBinaryTreeII.java
@@ -0,0 +1,28 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public int maxPathSum(TreeNode root) {
+    // Write your solution here.
+    int[] globalmax = {Integer.MIN_VALUE};
+    maxSubSum(root, globalmax);
+    return globalmax[0];
+  }
+  
+  private int maxSubSum(TreeNode root, int[] globalmax) {
+    if (root == null) {
+      return 0;
+    }
+    int sumLeft = maxSubSum(root.left, globalmax);
+    int sumRight = maxSubSum(root.right, globalmax);
+    globalmax[0] = Math.max(globalmax[0], sumLeft + sumRight + root.key);
+    return Math.max(0, root.key + Math.max(sumLeft, sumRight));
+  }
+}
