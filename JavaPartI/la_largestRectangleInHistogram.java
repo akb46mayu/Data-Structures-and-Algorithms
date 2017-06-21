7872a5b5f1363b315c081de9836749964c7285ea
akb46mayu <bofansunrise@gmail.com>
2017-06-21 18:17:02 -0400

    Create la_largestRectangleInHistogram.java

---
+++ JavaPartI/la_largestRectangleInHistogram.java
@@ -0,0 +1,18 @@
+public class Solution {
+  public int largest(int[] array) {
+    // Write your solution here.
+    Deque<Integer> stack = new LinkedList<Integer>();
+    int res = 0;
+    int n = array.length;
+    for (int i = 0; i <= n; i++) {
+      int curh = (i == n ? 0 : array[i]);
+      while (!stack.isEmpty() && array[stack.peekFirst()] > curh) {
+        int height = array[stack.pollFirst()];
+        int left = (stack.isEmpty() ? -1 : stack.peekFirst());
+        res = Math.max(res, (i - left - 1) * height);
+      }
+      stack.offerFirst(i);
+    }
+    return res;
+  }
+}
