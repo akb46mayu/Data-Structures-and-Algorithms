3b0cd35ae3e3a40275e2546e279df8bcb8aec665
akb46mayu <bofansunrise@gmail.com>
2017-06-25 15:15:57 -0400

    Create la_implementLRUCache.java

---
+++ JavaPartI/la_implementLRUCache.java
@@ -0,0 +1,85 @@
+public class Solution<K, V> {
+  static class Node<K, V> {
+    K key;
+    V val;
+    int limit;
+    Node<K, V> prev;
+    Node<K, V> next;
+    Node(K key, V val) {
+      this.key = key;
+      this.val = val;
+    }
+    void update(K key, V val) {
+      this.key = key;
+      this.val = val;
+    }
+  }
+  private int limit;
+  Node<K, V> head;
+  Node<K, V> tail;
+  Map<K, Node<K, V>> hmap;
+  // limit is the max capacity of the cache
+  public Solution(int limit) {
+    this.limit = limit;
+    this.hmap = new HashMap<K, Node<K, V>>();
+  }
+  
+  public void set(K key, V value) {
+    Node<K, V> node = null;
+    if (hmap.containsKey(key)) {
+      node = hmap.get(key);
+      node.val = value;
+      remove(node);
+    } else if (hmap.size() < limit) {
+      node = new Node<K, V>(key, value);
+    } else {
+      node = tail;
+      remove(node);
+      node.update(key, value);
+    }
+    moveToHead(node);
+  }
+  
+  public V get(K key) {
+    Node<K, V> node = hmap.get(key);
+    if (node != null) {
+      remove(node);
+      moveToHead(node);
+      return node.val;
+    } else {
+      return null;
+    }
+  }
+  
+  private Node<K, V> moveToHead(Node<K, V> node) {
+    // update hmap, move cur node to the head
+    hmap.put(node.key, node);
+    if (head == null) {
+      head = tail = node;
+    } else {
+      node.next = head;
+      head.prev = node;
+      head = node;
+    }
+    return node;
+  }
+  
+  private Node<K, V> remove(Node<K, V> node) {
+    // remove a node
+    hmap.remove(node.key);
+    if (node.prev != null) {
+      node.prev.next = node.next;
+    }
+    if (node.next != null) {
+      node.next.next = node.prev;
+    }
+    if (node == head) {
+      head = head.next;
+    }
+    if (node == tail) {
+      tail = tail.prev;
+    }
+    node.next = node.prev = null;
+    return node;
+  }
+}
