2bec89a9c24031283efa7eb1a253c77895cb9f87
akb46mayu <bofansunrise@gmail.com>
2017-06-29 10:56:56 -0400

    Update la_perfectShuffle.java

--- JavaPartI/la_perfectShuffle.java
+++ JavaPartI/la_perfectShuffle.java
@@ -16,3 +16,23 @@ public class Solution {
     arr[right] = temp;
   }
 }
+////////
+public class Solution2 {
+  public void shuffle(int[] array) {
+    // Write your solution here.
+    if (array.length <= 1) {
+      return;
+    }
+    int n = array.length;
+    for (int i = 0; i < n - 1; i++) {
+      int offset = (int)(Math.random() * (n - i));
+      swap(array, i + offset, i);
+    }
+  }
+  
+  private void swap(int[] arr, int left, int right) {
+    int temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+  }
+}
