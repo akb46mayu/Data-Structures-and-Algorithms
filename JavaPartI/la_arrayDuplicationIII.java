413de2103c9b1555559302bff67273aaef4bde03
akb46mayu <bofansunrise@gmail.com>
2017-07-07 11:54:07 -0400

    Update la_arrayDuplicationIII.java

--- JavaPartI/la_arrayDuplicationIII.java
+++ JavaPartI/la_arrayDuplicationIII.java
@@ -19,3 +19,28 @@ public class Solution {
     return Arrays.copyOf(array, i + 1);
   }
 }
+
+/// diff logic 
+
+public class Solution {
+  public int[] dedup(int[] array) {
+    // Write your solution here.
+    int n = array.length;
+    if(n <= 1) {
+      return array;
+    }
+    int i = 0;
+    for(int j = 1; j < n; j++) {
+      if(array[j] != array[j - 1]) {
+      	array[++i] = array[j];
+      } else {
+        while(j < n && array[j] == array[j - 1]) {
+          j++;
+        }
+        j--;
+        i--;
+      }
+    }
+    return Arrays.copyOf(array, i + 1);
+  }
+}
