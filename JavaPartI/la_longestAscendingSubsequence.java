f1fbf22721fdb780522ad111ae3d107f46f4fdcf
akb46mayu <bofansunrise@gmail.com>
2017-07-26 18:40:54 -0400

    Update la_longestAscendingSubsequence.java

--- JavaPartI/la_longestAscendingSubsequence.java
+++ JavaPartI/la_longestAscendingSubsequence.java
@@ -18,3 +18,27 @@ public class Solution {
     return glbmax;
   }
 }
+
+////// way 2
+public class Solution {
+  public int longest(int[] array) {
+    // Write your solution here.
+    int n = array.length;
+    if (n <= 1) {
+      return n;
+    }
+    int[] dp = new int[n];
+    dp[0] = 1;
+    int res = 1;
+    for (int i = 1; i < n; i++) {
+      dp[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (array[i] > array[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      res = Math.max(res, dp[i]);
+    }
+    return res;
+  }
+}
