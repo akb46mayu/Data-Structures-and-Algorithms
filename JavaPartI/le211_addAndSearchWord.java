2a47150eaf079b171ea4baf653d5a157ee29f7b6
akb46mayu <bofansunrise@gmail.com>
2017-07-30 17:58:47 -0400

    Update le211_addAndSearchWord.java

--- JavaPartI/le211_addAndSearchWord.java
+++ JavaPartI/le211_addAndSearchWord.java
@@ -16,7 +16,7 @@ search("b..") -> true
 Note:
 You may assume that all words are consist of lowercase letters a-z.
 */
-/// trie + dfs + ascii array
+/// way 1 trie + dfs for all possible cases + ascii array
 class TrieNode {
     TrieNode[] children = new TrieNode[26];
     boolean isWord;
@@ -78,3 +78,66 @@ public class WordDictionary {
  * obj.addWord(word);
  * boolean param_2 = obj.search(word);
  */
+
+
+//////////////      way2, dfs with pruning
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isWord;
+    TrieNode() {
+        
+    }
+}
+public class WordDictionary {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode cur = root;
+        for (char ch : word.toCharArray()) {
+            if (cur.children[ch - 'a'] == null) {
+                TrieNode newnode = new TrieNode();
+                cur.children[ch - 'a'] = newnode;
+                cur = newnode;
+            } else {
+                cur = cur.children[ch - 'a'];
+            }
+        }
+        cur.isWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        boolean[] res = {false};
+        find(word, 0, root, res);
+        return res[0];
+    }
+    
+    private void find(String word, int level, TrieNode root, boolean[] res) {
+        if (level == word.length()) {
+            res[0] = root.isWord;
+            return;
+        }
+        if (res[0] == true) {
+            return;
+        }
+        char ch = word.charAt(level);
+        if (ch == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (root.children[i] != null) {
+                    find(word, level + 1, root.children[i], res);
+                }
+            }
+            return;
+        } else if (root.children[ch - 'a'] != null) {
+            find(word, level + 1, root.children[ch - 'a'], res);
+        } else {
+            return;
+        }
+        
+    }
+}
