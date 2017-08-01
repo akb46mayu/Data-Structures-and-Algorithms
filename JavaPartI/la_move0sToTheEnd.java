0cffc600933059657ea4b083a9c1302590303c32
akb46mayu <bofansunrise@gmail.com>
2017-07-31 23:12:56 -0400

    Update la_move0sToTheEnd.java

--- JavaPartI/la_move0sToTheEnd.java
+++ JavaPartI/la_move0sToTheEnd.java
@@ -20,3 +20,30 @@ public class Solution {
     array[right] = temp;
   }
 }
+//////////////////////////////
+
+public class Solution {
+  public int[] moveZero(int[] array) {
+    // Write your solution here.
+    if (array.length == 0) {
+      return array;
+    }
+    int i = 0, j = array.length - 1;
+    while (i <= j) {
+      if (array[i] != 0) {
+        i++;
+      } else if (array[j] == 0) {
+        j--;
+      } else {
+        swap(array, i++, j--);
+      }
+    }
+    return array;
+  }  
+  
+  private void swap(int[] array, int left, int right){
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+  }
+}
