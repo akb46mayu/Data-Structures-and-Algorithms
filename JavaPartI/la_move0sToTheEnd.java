f3eebf5c4229ebcb609396dd62d5ca57e6b8fd11
akb46mayu <bofansunrise@gmail.com>
2017-07-31 23:08:53 -0400

    Update la_move0sToTheEnd.java

--- JavaPartI/la_move0sToTheEnd.java
+++ JavaPartI/la_move0sToTheEnd.java
@@ -1,32 +1,22 @@
 
-
-import java.util.Arrays;
-public class move0 {   // this method will change the order of array, althoug it can move all the elements to the right.
-	public int[] moveZero(int[] array) {
-	    // Write your solution here.
-	    if (array.length == 0){
-	      return array;
-	    }
-	    int zero = array.length - 1, i = 0;
-	    while (i <= zero){
-	      if (array[i] == 0){
-		swap(array, i, zero--);
-	      }
-	      i++;
-	    }
-	    return array;
-	  }  
-
-	private void swap(int[] array, int left, int right){
-	int temp = array[left];
-	array[left] = array[right];
-	array[right] = temp;
-	}
-	public static void main(String[] args){
-	  move0 a = new move0();
-	  int[] array = {2,4,1,0,0,7,3};
-	  a.moveZero(array);
-	  System.out.println(Arrays.toString(array));
-	}
-
+public class Solution {
+  public int[] moveZero(int[] array) {
+    // Write your solution here.
+    if (array.length == 0) {
+      return array;
+    }
+    int i = 0, n = array.length;
+    for (int j = 0; j < n; j++) {
+      if (array[j] != 0) {
+        swap(array, i++, j);
+      }
+    }
+    return array;
+  }  
+  
+  private void swap(int[] array, int left, int right){
+    int temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+  }
 }
