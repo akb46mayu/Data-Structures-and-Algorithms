5fb328ffff2a9a55c4f6aa859c2f06e7b1d435b1
akb46mayu <bofansunrise@gmail.com>
2017-08-04 15:25:42 -0400

    Update le73_setMatrixZeros.java

--- JavaPartI/le73_setMatrixZeros.java
+++ JavaPartI/le73_setMatrixZeros.java
@@ -1,4 +1,4 @@
-Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+/* Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 
 click to show follow up.
 
@@ -6,7 +6,7 @@ Follow up:
 Did you use extra space?
 A straight forward solution using O(mn) space is probably a bad idea.
 A simple improvement uses O(m + n) space, but still not the best solution.
-Could you devise a constant space solution?
+Could you devise a constant space solution?  */
 
 public class Solution {  // S = O(m + n)
     public void setZeroes(int[][] matrix) {
@@ -30,6 +30,48 @@ public class Solution {  // S = O(m + n)
                     matrix[i][j] = 0;
                 }
             } 
+        }
+    }
+}
+///// S = O (1)
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null) {
+            return;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        boolean coltag = false, rowtag = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0) {
+                        rowtag = true;
+                    } 
+                    if (j == 0) {
+                        coltag = true;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            } 
+        }
+        if (rowtag) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (coltag) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 }
