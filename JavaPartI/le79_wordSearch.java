daabc3b3875998d4512d81c7adb8b79ab562c1b8
akb46mayu <bofansunrise@gmail.com>
2017-08-05 12:02:13 -0400

    Create le79_wordSearch.java

---
+++ JavaPartI/le79_wordSearch.java
@@ -0,0 +1,74 @@
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,
+Given board =
+
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+
+
+*/
+
+
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        if (m == 0 || word.length() == 0) {
+            return false;
+        }
+        int n = board[0].length;
+        boolean[] res = {false};
+        int[][] visited = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                existHelper(board, i, j, word, 0, res, visited);
+                if (res[0] == true) {
+                    return res[0];
+                }
+            }
+        }
+        return res[0];
+    }
+    
+    private void existHelper(char[][] board, int x, int y, String word, int level, boolean[] res, int[][] visited) {
+        if (res[0] == true) {
+            return;
+        }
+        int m = board.length, n = board[0].length;
+        char ch = board[x][y];
+       
+        if (level == word.length() - 1) {
+            if (ch == word.charAt(level) && visited[x][y] == 0) {
+                 res[0] = true;
+                 
+            }
+            return;
+        }
+        
+        if (ch != word.charAt(level)) {
+            return;
+        }
+        visited[x][y] = 1;
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        for (int k = 0; k < 4; k++) {
+            int newx = x + dx[k];
+            int newy = y + dy[k];
+            if (newx >= 0 && newx < m && newy >= 0 && newy < n && visited[newx][newy] == 0) {
+                existHelper(board, newx, newy, word, level + 1, res, visited);
+                
+            }
+        }
+        visited[x][y] = 0;
+    }
+    
+}
