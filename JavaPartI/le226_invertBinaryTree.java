2eb2903efce19bc37b6cfd8aa0f55d9ffedf7855
akb46mayu <bofansunrise@gmail.com>
2017-08-06 15:42:34 -0400

    Create le226_invertBinaryTree.java

---
+++ JavaPartI/le226_invertBinaryTree.java
@@ -0,0 +1,45 @@
+/* Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return root;
+        }
+   
+        if (root.left != null && root.right != null) {
+            TreeNode nleft = invertTree(root.right);
+            TreeNode nright = invertTree(root.left);
+            root.left = nleft;
+            root.right = nright;
+        } else if (root.left != null) {
+            root.right = invertTree(root.left);
+            root.left = null;
+        } else {
+            root.left = invertTree(root.right);
+            root.right = null;
+        }
+        return root;
+    }
+}
