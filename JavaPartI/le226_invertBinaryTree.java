df0b5e54a70f2c2bcde104c8d0951557b2199370
akb46mayu <bofansunrise@gmail.com>
2017-08-06 15:50:43 -0400

    Update le226_invertBinaryTree.java

--- JavaPartI/le226_invertBinaryTree.java
+++ JavaPartI/le226_invertBinaryTree.java
@@ -23,6 +23,22 @@ to
  * }
  */
 // recursion witnout return values
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+       invertHelper(root);
+       return root;
+    }
+    private void invertHelper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertHelper(root.left);
+        invertHelper(root.right);
+    }
+}
 
 ///   recursion with return values
 public class Solution {
