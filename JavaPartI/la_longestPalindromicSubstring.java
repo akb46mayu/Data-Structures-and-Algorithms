9215e17ed1de3bd4f4d7a84a0367f63a59944880
akb46mayu <bofansunrise@gmail.com>
2017-08-11 17:47:07 -0400

    Update la_longestPalindromicSubstring.java

--- JavaPartI/la_longestPalindromicSubstring.java
+++ JavaPartI/la_longestPalindromicSubstring.java
@@ -73,3 +73,39 @@ public class Solution {
 /////////////////////////////////////////////// the if else blocks for l, r can also be condensed as:
 int l = i / 2;
 int r = i % 2 == 0 ? l : l + 1;
+
+
+///  solution 2 dp
+
+public class Solution {
+  public String longestPalindrome(String s) {
+    String res = "";
+    int n = s.length();
+    if (n == 0) {
+      return res;
+    }
+    if (n == 1) {
+      return s.substring(0, 1);
+    }
+    int[][] dp = new int[n][n];
+    int start = 0;
+    int mval = 1;
+    for (int j = 1; j < n; j++) {
+      dp[j][j] = 1;
+      for (int i = j - 1; i >= 0; i--) {
+        if (s.charAt(j) == s.charAt(i)) {
+          if (i + 1 == j || dp[i + 1][j - 1] == 1) {
+            dp[i][j] = 1;
+            int len = j - i + 1;
+            if (len > mval) {
+              mval = len;
+              start = i;
+            }
+          }
+        }
+      }
+    }
+    int right = start + mval - 1;
+    return s.substring(start, right + 1);
+  }
+}
