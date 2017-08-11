b749b1865b7568cae8e2ea34fbf7834ea85f6898
akb46mayu <bofansunrise@gmail.com>
2017-08-11 12:03:15 -0400

    Update lae_decodeWays.java

--- JavaPartI/lae_decodeWays.java
+++ JavaPartI/lae_decodeWays.java
@@ -1,26 +1,24 @@
 
 
 public class Solution {
-   public int numDecodeWay(String s) {
-     //Input your code here
-     if (s == null || s.length() == 0) {
-       return 0;
-     }
-     int n = s.length();
-     int[] dp = new int[n + 1];
-     dp[0] = 1;
-     dp[1] = s.charAt(0) == '0' ? 0 : 1;
-     // base case
-     for (int j = 2; j <= n; j++) {
-       int one = s.charAt(j - 1) -'0';
-       int two = one + 10 * (s.charAt(j - 2) -'0');
-       if (one >= 1 && one <= 9) {
-        dp[j] += dp[j - 1];
-       }
-       if (two >= 10 && two <= 26) {
-         dp[j] += dp[j - 2];
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-     }
-     return dp[n];
-  }
-}
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        // base case
+        for (int j = 2; j <= n; j++) {
+            int one = s.charAt(j - 1) -'0';
+            int two = one + 10 * (s.charAt(j - 2) -'0');
+            if (one >= 1 && one <= 9) {
+             dp[j] += dp[j - 1];
+            }
+            if (two >= 10 && two <= 26) {
+             dp[j] += dp[j - 2];
+            }
+        }
+        return dp[n];
+    }
