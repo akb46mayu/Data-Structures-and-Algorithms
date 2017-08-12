f61ba677aa981ed55d565a9f6a5de71e76e4bc8a
akb46mayu <bofansunrise@gmail.com>
2017-08-12 11:06:39 -0400

    Update la_minimumCutsForPalindromes.java

--- JavaPartI/la_minimumCutsForPalindromes.java
+++ JavaPartI/la_minimumCutsForPalindromes.java
@@ -28,3 +28,35 @@ public class Solution {
     return mincut[n - 1];
   }
 }
+//// way 2 diff logic 
+public class Solution {
+  public int minCuts(String input) {
+    // Write your solution here.
+    int n = input.length();
+    if (n <= 1) {
+      return 0;
+    }
+    boolean[][] pal = new boolean[n][n];
+    int[] dp = new int[n];
+    for (int j = 1; j < n; j++) {
+      dp[j] = j;
+      for (int i = j; i >= 0; i--) {
+        if (input.charAt(i) == input.charAt(j)) {
+          if (j - i <= 1) {
+            pal[i][j] = true;
+          } else {
+            pal[i][j] = pal[i + 1][j - 1] == true ? true : false;
+          }
+        }
+        if (pal[i][j] == true) {
+          if (i == 0) {
+            dp[j] = 0;
+          } else {
+            dp[j] = Math.min(dp[j], dp[i - 1] + 1);
+          }
+        }
+      }
+    }
+    return dp[n - 1];
+  }
+}
