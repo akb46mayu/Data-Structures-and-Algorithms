4de3d1dccbc12b4c92280243f070abe50f7092e0
akb46mayu <bofansunrise@gmail.com>
2017-08-26 12:37:51 -0400

    Update le345_reverseVowelsOfAString.java

--- JavaPartI/le345_reverseVowelsOfAString.java
+++ JavaPartI/le345_reverseVowelsOfAString.java
@@ -29,3 +29,44 @@ class Solution {
         return false;
     }
 }
+//// sol use set
+class Solution2 {
+    public String reverseVowels(String s) {
+        int n = s.length();
+        int i = 0, j = n - 1;
+        
+        char[] arr = s.toCharArray();
+        Set<Character> hset = generateVow();
+        while(i < j) {
+            while (i < j && !hset.contains(arr[i])) {
+                i++;
+            }
+            while (i < j && !hset.contains(arr[j])) {
+                j--;
+            }
+            swap(arr, i++, j--);
+        }
+        return new String(arr);
+    }
+    
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    private Set<Character> generateVow() {
+        Set<Character> hset = new HashSet<>();
+        hset.add('a');
+        hset.add('e');
+        hset.add('i');
+        hset.add('o');
+        hset.add('u');
+        hset.add('A');
+        hset.add('E');
+        hset.add('I');
+        hset.add('O');
+        hset.add('U');
+        return hset;
+    } 
+}
