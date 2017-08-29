7942feb93d0fc80146a85856a732994eb2ba7651
akb46mayu <bofansunrise@gmail.com>
2017-08-29 14:28:35 -0400

    Update le141_linkedListCycle.java

--- LinkedList/le141_linkedListCycle.java
+++ LinkedList/le141_linkedListCycle.java
@@ -12,12 +12,12 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head, slow = head;
-        while (fast!=null && fast.next!= null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (slow == fast) {
-                return true;
-            }
+        while (fast != null && fast.next != null) {
+          slow = slow.next;
+          fast = fast.next.next;
+          if (fast == slow) {
+            return true;
+          }
         }
         return false;
     }
