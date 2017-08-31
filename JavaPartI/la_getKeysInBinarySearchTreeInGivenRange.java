761c40124f03c8587787baf032c2df664f16875c
akb46mayu <bofansunrise@gmail.com>
2017-08-31 18:41:24 -0400

    Update la_getKeysInBinarySearchTreeInGivenRange.java

--- JavaPartI/la_getKeysInBinarySearchTreeInGivenRange.java
+++ JavaPartI/la_getKeysInBinarySearchTreeInGivenRange.java
@@ -27,3 +27,28 @@ public class Solution {
     rangeHelper(root.right, min, max, result);
   }
 }
+
+//// solution two with cutting branches
+public class Solution {
+  public List<Integer> getRange(TreeNode root, int min, int max) {
+    // Write your solution here.
+    List<Integer> res = new ArrayList<>();
+    rangeHelper(root, min, max, res);
+    return res;
+  }
+  
+  private void rangeHelper(TreeNode root, int min, int max, List<Integer> res) {
+    if (root == null) {
+      return;
+    }
+    if (root.key > min) {
+      rangeHelper(root.left, min, max, res);
+    }
+    if (root.key >= min && root.key <= max) {
+      res.add(root.key);
+    }
+    if (root.key < max) {
+      rangeHelper(root.right, min, max, res);
+    }
+  }
+}
