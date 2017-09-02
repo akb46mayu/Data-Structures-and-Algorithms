de9f6e50608db83bbd6c1a3dd465591ea9a8436d
akb46mayu <bofansunrise@gmail.com>
2017-09-02 11:44:56 -0400

    Update le63_uniquePathsII.java

--- JavaPartI/le63_uniquePathsII.java
+++ JavaPartI/le63_uniquePathsII.java
@@ -1,4 +1,3 @@
-// way 1
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
@@ -6,8 +5,6 @@ class Solution {
             return 0;
         }
         int n = obstacleGrid[0].length;
-        boolean rowtag = false;
-        boolean coltag = false;
         int[][] dp = new int[m][n];
         if (obstacleGrid[0][0] == 1) {
             return 0;
@@ -18,27 +15,9 @@ class Solution {
                 if (i == 0 && j == 0) {
                     dp[0][0] = 1;
                 } else if (i == 0) {
-                    if (rowtag == false) {
-                        if (cur == 1) {
-                            rowtag = true;
-                            dp[i][j] = 0;
-                        } else {
-                            dp[i][j] = 1;
-                        }
-                    } else {
-                        dp[i][j] = 0;
-                    }
+                    dp[i][j] = cur == 1 ? 0 : dp[i][j - 1];
                 } else if (j == 0) {
-                    if (coltag == false) {
-                        if (cur == 1) {
-                            coltag = true;
-                            dp[i][j] = 0;
-                        } else {
-                            dp[i][j] = 1;
-                        }
-                    } else {
-                        dp[i][j] = 0;
-                    }
+                    dp[i][j] = cur == 1 ? 0 : dp[i - 1][j];
                 } else {
                     dp[i][j] = cur != 1 ? dp[i - 1][j] + dp[i][j - 1] : 0;
                 }
