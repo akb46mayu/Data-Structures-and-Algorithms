900a7f25db7c9d46b3f360ab10f8e248a0573512
akb46mayu <bofansunrise@gmail.com>
2017-09-02 10:59:10 -0400

    Create le64_minimumPathSum.java

---
+++ JavaPartI/le64_minimumPathSum.java
@@ -0,0 +1,31 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+*/
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int res = 0;
+        if (m == 0) {
+            return res;
+        }
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
