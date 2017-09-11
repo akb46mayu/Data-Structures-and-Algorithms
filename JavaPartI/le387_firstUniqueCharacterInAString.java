cd4e5464b02e10083bab977a7033b90202a412df
akb46mayu <bofansunrise@gmail.com>
2017-09-11 17:20:01 -0400

    Update le387_firstUniqueCharacterInAString.java

--- JavaPartI/le387_firstUniqueCharacterInAString.java
+++ JavaPartI/le387_firstUniqueCharacterInAString.java
@@ -11,57 +11,46 @@ Note: You may assume the string contain only lowercase letters.
 
 */
 // my initial solution, can deal with any chars not only 256 cases.
-class Point {
-    int idx;
-    int count;
-    Point(int idx, int count) {
-        this.idx = idx;
-        this.count = count;
-    }
-}
-
-public class Solution {
+class Solution {
     public int firstUniqChar(String s) {
-        if (s == null || s.length() == 0) {
+        if (s == null) {
             return -1;
         }
-        Map<Character, Point> hmap = new HashMap<>();
         int n = s.length();
-        int res = Integer.MAX_VALUE;
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            Point temp = hmap.get(ch);
-            if (temp != null) {
-                temp.count += 1;
+            Integer count = map.get(ch);
+            if (count != null) {
+                map.put(ch, count + 1);
             } else {
-                hmap.put(ch, new Point(i, 1));
+                map.put(ch, 1);
             }
         }
-        int tag = 0;
-        for (Point pt : hmap.values()) {
-            if (pt.count == 1 && pt.idx < res) {
-                res = pt.idx;
-                tag = 1;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (map.get(ch) == 1) {
+                return i;
             }
         }
-        return tag == 1 ? res : -1;
+        return -1;
     }
 }
 
 
 ////////////////////////////// (not using hash, use an int array)
-public class Solution2 {
+public class Solution {
     public int firstUniqChar(String s) {
         if (s == null || s.length() == 0) {
             return -1;
         }
-        int[] dict = new int[256];
+        int[] dict = new int[26];
         for (char ch : s.toCharArray()) {
-            dict[ch]++;
+            dict[ch - 'a']++;
         }
         int n = s.length();
         for (int i = 0; i < n; i++) {
-            if (dict[s.charAt(i)] == 1) {
+            if (dict[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
