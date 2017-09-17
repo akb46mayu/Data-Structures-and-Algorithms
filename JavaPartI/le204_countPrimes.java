87ec0586e3bdc579b0383930ae8ea26f72013adf
akb46mayu <bofansunrise@gmail.com>
2017-09-17 11:27:15 -0400

    Update le204_countPrimes.java

--- JavaPartI/le204_countPrimes.java
+++ JavaPartI/le204_countPrimes.java
@@ -17,3 +17,31 @@ class Solution {
         return count;
     }
 }
+
+
+// way 2
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] check =  new boolean[n]; // true: prime
+        Arrays.fill(check, true);
+        if (n <= 2) {
+            return count;
+        }
+        check[0] = false;
+        check[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (check[i] == true) {
+               for (int j = i; j * i <= n - 1; j++) {
+                   check[j * i] = false;
+               } 
+            }
+        }
+        for (boolean j : check) {
+            if (j == true) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
