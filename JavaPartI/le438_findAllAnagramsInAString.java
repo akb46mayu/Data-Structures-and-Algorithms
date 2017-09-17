28a8451cb43286c045a9aa0ec338a80d0d2dd07a
akb46mayu <bofansunrise@gmail.com>
2017-09-17 15:02:05 -0400

    Update le438_findAllAnagramsInAString.java

--- JavaPartI/le438_findAllAnagramsInAString.java
+++ JavaPartI/le438_findAllAnagramsInAString.java
@@ -35,3 +35,38 @@ class Solution {
         return res;
     }
 }
+
+/// way II 
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int[] dict = new int[256];
+        int numele = 0;
+        int n = s.length();
+        for (char ch : p.toCharArray()) {
+            dict[ch]++;
+            if (dict[ch] == 1) {
+                numele++;
+            }
+        }
+        int k = p.length(); // window size
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            dict[ch]--;
+            if ( dict[ch] == 0) {
+                numele--;
+            }
+            if (i >= k) {
+                char prev = s.charAt(i - k);
+                dict[prev]++;
+                if ( dict[prev] == 1) {
+                    numele++;
+                } 
+            }            
+            if (numele == 0) {
+                res.add(i - k + 1);
+            }
+        }
+        return res;
+    }
+}
