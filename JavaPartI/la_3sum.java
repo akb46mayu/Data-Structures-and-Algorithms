e37e59914bdf8ed5decb641a6c51886205126893
akb46mayu <bofansunrise@gmail.com>
2017-09-26 20:55:52 -0400

    Update la_3sum.java

--- JavaPartI/la_3sum.java
+++ JavaPartI/la_3sum.java
@@ -27,3 +27,34 @@ public class Solution {
     return res;
   }
 }
+
+//// mehtod 2
+public class Solution {
+  public List<List<Integer>> allTriples(int[] array, int target) {
+    // Write your solution here.
+    List<List<Integer>> res = new ArrayList<>();
+    Set<Integer> set = new HashSet<>();
+    int n = array.length;
+    Arrays.sort(array);
+    for(int i = 0; i < n - 2; i++) {
+      if (!set.contains(array[i])) {
+        set.add(array[i]);
+        int tar = target - array[i];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j = i + 1; j < n; j++) {
+          Integer count = map.get(tar - array[j]);
+          if (count != null) {
+            if (count == 1) {
+              res.add(Arrays.asList(array[i], tar - array[j], array[j]));
+              map.put( tar - array[j], count - 1);
+              map.put(array[j], 0);
+            }
+          } else {
+            map.put(array[j], 1);
+          }
+        }
+      } 
+    }
+    return res;
+  }
+}
