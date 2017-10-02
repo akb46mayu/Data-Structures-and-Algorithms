abd8fc3078713d96d52d3bf08a78e01a1862b9ec
akb46mayu <bofansunrise@gmail.com>
2017-10-01 22:18:59 -0400

    Update la_maximumValuesOfSizeKSlidingWindows.java

--- JavaPartI/la_maximumValuesOfSizeKSlidingWindows.java
+++ JavaPartI/la_maximumValuesOfSizeKSlidingWindows.java
@@ -19,3 +19,47 @@ public class Solution {
     return res;
   }
 }
+
+/// method 2: with pq 
+class Pair {
+  int val;
+  int idx;
+  Pair(int val, int idx) {
+    this.val = val;
+    this.idx = idx;
+  }
+}
+
+class MyComparator implements Comparator<Pair> {
+  public int compare(Pair o1, Pair o2) {
+    if (o1.val == o2.val) {
+      return 0;
+    }
+    return o2.val < o1.val ? -1 : 1;
+  }
+}
+public class Solution {
+  public List<Integer> maxWindows(int[] array, int k) {
+    // Write your solution here.
+    Queue<Pair> pq = new PriorityQueue<>(1, new MyComparator());
+    List<Integer> res = new ArrayList<>();
+    int n = array.length;
+    for (int i = 0; i < n; i++) {  // i is the right idx of the sliding window
+      if (i < k) {
+        pq.offer(new Pair(array[i], i));
+        if (i == k - 1) {
+            res.add(pq.peek().val);
+        }
+        continue;
+      }
+      Pair temp = pq.peek();
+      int idx = temp.idx;
+      if (idx <= i - k) {
+        pq.poll();
+      }
+      pq.offer(new Pair(array[i], i));
+      res.add(pq.peek().val);
+    }
+    return res;
+  }
+}
