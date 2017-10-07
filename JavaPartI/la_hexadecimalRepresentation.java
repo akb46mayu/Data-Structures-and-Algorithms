b517f68e9283fa6cb4819b1f5643870c240ee0d5
akb46mayu <bofansunrise@gmail.com>
2017-10-07 00:19:48 -0400

    Update la_hexadecimalRepresentation.java

--- JavaPartI/la_hexadecimalRepresentation.java
+++ JavaPartI/la_hexadecimalRepresentation.java
@@ -18,3 +18,24 @@ public class Solution {
     return prefix + sb.reverse().toString();
   }
 }
+//// way2
+public class Solution {
+  public String hex(int number) {
+    // Write your solution here.
+    StringBuilder sb = new StringBuilder();
+    if (number == 0) {
+      sb.append("0");
+    }
+    while (number >= 1) {
+      int quo = number % 16;
+      if (quo <= 9) {
+        sb.append((char)(quo + '0'));
+      } else {
+        sb.append((char)('A' + quo - 10));
+      }
+      number = number >> 4;
+    }
+    sb.append("x0");
+    return sb.reverse().toString();
+  }
+}
