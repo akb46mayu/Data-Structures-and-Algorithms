443e960153907c339040819ed4b08e3cfb04cf4d
akb46mayu <bofansunrise@gmail.com>
2018-03-14 17:55:24 -0400

    Update la_kSmallestInUnsortedArray.py

--- pythonPart/la_kSmallestInUnsortedArray.py
+++ pythonPart/la_kSmallestInUnsortedArray.py
@@ -18,6 +18,32 @@ class Solution(object):
     return res  
 
 
+#############################################
+class Solution2(object):
+  def kSmallest(self, array, k):
+    """
+    input: int[] array, int k
+    return: int[]
+    """
+    # write your solution here
+    pq = [] # min heap with neg values (max heap)
+    if len(array) == 0 or k <= 0:
+      return []
+    res = [0] * k
+    i = 0
+    for ele in array:
+      if i < k:
+        heappush(pq, -ele)
+      else:
+      	if -pq[0] > ele:
+          heappop(pq)
+          heappush(pq, -ele)
+      i += 1
+    while k > 0:
+      res[k - 1] = -heappop(pq)
+      k -= 1
+    return res
+
 array = [5,23,2,1,4,7,8,9]
 k = 3
 sol = Solution()
