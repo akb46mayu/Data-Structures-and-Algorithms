7c97b77e8de1fd59258d4a32a24f73b82acddd60
akb46mayu <bofansunrise@gmail.com>
2018-03-15 11:58:40 -0400

    Update le223_rectangleArea.py

--- Math/le223_rectangleArea.py
+++ Math/le223_rectangleArea.py
@@ -40,3 +40,27 @@ class Solution(object):
             out = 0
         return out
 # EASIER SOLUTION LOOK the others
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        area1 = abs((A -C) * (B - D))
+        area2 = abs((E - G) * (F - H))
+        if C < E or G < A or B > H or F > D:
+            return area1 + area2
+        else:
+            list1 = [A, C, E, G]
+            list2 = [B, D, F, H]
+            list1.sort()
+            list2.sort()
+            area3 = (list1[2] - list1[1]) * (list2[2] - list2[1])
+            return area1 + area2 - area3
