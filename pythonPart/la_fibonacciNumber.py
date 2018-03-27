0b394019b3352395998a014005c88490339e1fa6
akb46mayu <bofansunrise@gmail.com>
2018-03-26 22:31:18 -0400

    Update la_fibonacciNumber.py

--- pythonPart/la_fibonacciNumber.py
+++ pythonPart/la_fibonacciNumber.py
@@ -31,3 +31,31 @@ class Solution2(object): # T = O(k), S = O(1)
     for i in range(2, K + 1):
       f[i % 2] = f[(i - 1) % 2] + f[(i - 2) % 2]
     return f[i % 2]
+
+  
+# recursion
+class Solution(object):
+  def __init__(self):
+    self.dict = {}
+    
+  def fibonacci(self, K):
+    
+    """
+    input: int K
+    return: long
+    """
+    # write your solution here
+    if K in self.dict:
+      return self.dict[K]
+    if K <= 0:
+      self.dict[0] = 0
+      return 0
+    if K == 1:
+      self.dict[1] = 1
+      return 1
+    
+    res = self.fibonacci(K - 1) + self.fibonacci(K - 2)
+    self.dict[K] = res
+    return res
+     
+    
