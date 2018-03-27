2508e66b6fcaa0ec2f781b4ce5fd0292a593c621
akb46mayu <bofansunrise@gmail.com>
2018-03-26 23:00:13 -0400

    Create la_aToThePowerOfB.py

---
+++ pythonPart/la_aToThePowerOfB.py
@@ -0,0 +1,19 @@
+class Solution(object):
+  def power(self, a, b):
+    """
+    input: int a, int b
+    return: long
+    """
+    # write your solution here
+    if b == 0:
+      return 1
+    if a == 0:
+      return 0
+    if a == 1:
+      return 1
+    if b % 2 == 1:
+      c = self.power(a, b // 2)
+      return a * c * c
+    else:
+      c = self.power(a, b // 2)
+      return c * c
