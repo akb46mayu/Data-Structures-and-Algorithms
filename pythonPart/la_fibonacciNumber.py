776a0ea7d9d4328f530e717d07277fb4b3501954
akb46mayu <bofansunrise@gmail.com>
2018-03-26 22:01:03 -0400

    Update la_fibonacciNumber.py

--- pythonPart/la_fibonacciNumber.py
+++ pythonPart/la_fibonacciNumber.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  # T = O(k), S = O(K)
   def fibonacci(self, K):
     """
     input: int K
@@ -14,3 +14,20 @@ class Solution(object):
     for i in range(2, K + 1):
       f[i] = f[i - 1] + f[i - 2]
     return f[K]
+
+  
+class Solution2(object): # T = O(k), S = O(1)
+  def fibonacci(self, K):
+    """
+    input: int K
+    return: long
+    """
+    # write your solution here
+    if K <= 0:
+      return 0
+    if K == 1:
+      return 1
+    f = [0, 1]
+    for i in range(2, K + 1):
+      f[i % 2] = f[(i - 1) % 2] + f[(i - 2) % 2]
+    return f[i % 2]
