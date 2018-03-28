3feb948b3b5bf924e4b3e5090eda92ae153f4e51
akb46mayu <bofansunrise@gmail.com>
2018-03-28 17:58:03 -0400

    Update la_getKeysInBinarySearchTreeInGivenRange.py

--- pythonPart/la_getKeysInBinarySearchTreeInGivenRange.py
+++ pythonPart/la_getKeysInBinarySearchTreeInGivenRange.py
@@ -16,3 +16,28 @@ class Solution(object):  ## O(n)
     if root.val >= min and root.val <= max:
       res.append(root.val)
     self.rangeHelper(root.right, min, max, res)
+
+    
+    
+    
+    
+class Solution2(object): # with prunning, T = O(logn)
+  def getRange(self, root, min, max):
+    """
+    input: TreeNode root, int min, int max
+    return: List<Integer>
+    """
+    # write your solution here
+    res = []
+    self.rangeHelper(root, min, max, res)
+    return res
+    
+  def rangeHelper(self, root, min, max, res):
+    if root is None:
+      return res
+    if root.val > min:
+      self.rangeHelper(root.left, min, max, res)
+    if root.val >= min and root.val <= max:
+      res.append(root.val)
+    if root.val < max:
+      self.rangeHelper(root.right, min, max, res)
