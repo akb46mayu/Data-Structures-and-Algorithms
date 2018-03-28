080dc82e973f680361dafe687a0b7cd747f7403f
akb46mayu <bofansunrise@gmail.com>
2018-03-28 15:03:00 -0400

    Create le94_binaryTreeInorderTraversal.py

---
+++ pythonPart/le94_binaryTreeInorderTraversal.py
@@ -0,0 +1,28 @@
+## non recursion# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = [] #int
+        stack = [] #node
+        if root is None:
+            return res
+    
+        cur = root
+        while stack or cur:
+            if cur != None:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right
+        return res
