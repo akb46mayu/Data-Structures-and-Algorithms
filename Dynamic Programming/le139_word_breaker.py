0b8dcce865bf084ca4bd4abf8c188300ccad3731
akb46mayu <bofansunrise@gmail.com>
2018-04-01 18:57:14 -0400

    Update le139_word_breaker.py

--- Dynamic Programming/le139_word_breaker.py
+++ Dynamic Programming/le139_word_breaker.py
@@ -8,7 +8,31 @@ dict = ["leet", "code"].
 Return true because "leetcode" can be segmented as "leet code".
 """
 
-
+class Solution(object):  # dp solution left big seg, right small seg
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        hset = collections.Counter(wordDict)
+        n = len(s)
+        dp = [False] * n
+        if n == 1:
+            return s in hset
+        dp[0] = True if s[0] in hset else False
+        for j in range(1, n):
+            for i in range(0, j + 1):
+                if i == 0:
+                    if s[:j+1] in hset:
+                        dp[j] = True
+                        break
+                else:
+                    if dp[i - 1] and (s[i:j+1] in hset):
+                        dp[j] = True
+                        break
+        return dp[n - 1]
+    ####################################
 
 class Solution(object):
     def wordBreak(self, s, wordDict):
