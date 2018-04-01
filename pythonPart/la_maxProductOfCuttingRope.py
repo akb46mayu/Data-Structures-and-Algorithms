450de893f692788aefc603805b81e3712e2c8c62
akb46mayu <bofansunrise@gmail.com>
2018-04-01 12:13:59 -0400

    Update la_maxProductOfCuttingRope.py

--- pythonPart/la_maxProductOfCuttingRope.py
+++ pythonPart/la_maxProductOfCuttingRope.py
@@ -14,3 +14,22 @@ class Solution(object): # method1: left big seg, right big seg
         r = i - l
         M[i] = max(M[i], max(l, M[l]) * max(r, M[r]))
     return M[n]
+
+  
+  
+class Solution(object): # method2: left big seg, right small seg
+  def maxProduct(self, length):
+    """
+    input: int length
+    return: int
+    """
+    # write your solution here
+
+    n = length
+    M = [0] * (n + 1)
+    M[1] = 1
+    for i in range(2, n + 1):  # total length of each case
+      for l in range(1, n):
+        r = i - l
+        M[i] = max(M[i], max(l, M[l]) * r)
+    return M[n]
