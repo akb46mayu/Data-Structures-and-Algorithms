c62935b3c1a39a7cd1accc0753f95ba96ad5544a
akb46mayu <bofansunrise@gmail.com>
2018-04-01 10:17:25 -0400

    Update la_longestAscendingSubarray.py

--- pythonPart/la_longestAscendingSubarray.py
+++ pythonPart/la_longestAscendingSubarray.py
@@ -1,4 +1,26 @@
-class Solution(object):
+class Solution(object):   # Greedy: T = O(n), S = O(1)
+  def longest(self, array):
+    """
+    input: int[] array
+    return: int
+    """
+    # write your solution here
+
+    if array is None or len(array) == 0:
+      return 0
+    count = 1
+    maxval = 1
+    n = len(array)
+    for i in range(1, n):
+      if array[i] > array[i - 1]:
+        count += 1
+        maxval = max(maxval, count)
+      else:
+        count = 1
+    return maxval
+
+##########################################################
+class Solution2(object): # Greedy: T = O(n), S = O(n)
   def longest(self, array):
     """
     input: int[] array
