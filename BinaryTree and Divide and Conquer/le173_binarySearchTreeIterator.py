14dfd9358e2f2410eba009ac5c5f8a386f6859db
akb46mayu <bofansunrise@gmail.com>
2018-04-04 21:21:13 -0400

    Update and rename le173_binary_search_tree_iterator.py to le173_binarySearchTreeIterator.py

---
+++ BinaryTree and Divide and Conquer/le173_binarySearchTreeIterator.py
@@ -0,0 +1,39 @@
+# Definition for a  binary tree node
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        self.pushAll(root)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) != 0
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if self.hasNext():
+            temp = self.stack.pop()
+            out = temp.val
+            self.pushAll(temp.right)
+            return out
+            
+    def pushAll(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+# Your BSTIterator will be called like this:
+# i, v = BSTIterator(root), []
+# while i.hasNext(): v.append(i.next())
