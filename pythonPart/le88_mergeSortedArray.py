6dc6fbefbf66a61b7c67998d9ea77074fc19637f
akb46mayu <bofansunrise@gmail.com>
2018-05-03 23:40:42 -0400

    Create le88_mergeSortedArray.py

---
+++ pythonPart/le88_mergeSortedArray.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while i > -1 and j > - 1:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
+        while j > - 1:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
