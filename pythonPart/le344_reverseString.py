920ea3f71e36287897853553b031504d23fc2f62
akb46mayu <bofansunrise@gmail.com>
2018-05-05 15:09:44 -0400

    Update le344_reverseString.py

--- pythonPart/le344_reverseString.py
+++ pythonPart/le344_reverseString.py
@@ -13,3 +13,22 @@ class Solution(object):
             i += 1
             j -= 1
         return ''.join(a)
+
+    
+## use a stack
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if s is None or len(s) <= 1:
+            return s
+        a = list(s)
+        stack = []
+        res = []
+        for ele in a:
+            stack.append(ele)
+        while stack:
+            res.append(stack.pop())
+        return ''.join(res)
